96a7352cb48e0b2252e6c4442b8d1b247446a6eb
Matias Benedetto <matias.benedetto@gmail.com>
2024-06-19 13:29:34 +1000

    Add lib-font credits and license in source code (#60973)
    * add lib-font credits and license in source code
    
    * undo js lint
    
    ---------
    
    Co-authored-by: matiasbenedetto <mmaattiiaass@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: mikachan <mikachan@git.wordpress.org
    
    Source: https://github.com/WordPress/gutenberg/commit/2222abbf1bf2784808090f084270fc09039ebcb0

--- build/edit-site/index.js
+++ build/edit-site/index.js
@@ -524,6 +524,36 @@ if (true) {
 /***/ 1497:
 /***/ ((module) => {
 
+/**
+ * Credits:
+ *
+ * lib-font
+ * https://github.com/Pomax/lib-font
+ * https://github.com/Pomax/lib-font/blob/master/lib/inflate.js
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 pomax@nihongoresources.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /* eslint eslint-comments/no-unlimited-disable: 0 */
 /* eslint-disable */
 /* pako 1.0.10 nodeca/pako */(function(f){if(true){module.exports=f()}else { var g; }})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c=undefined;if(!f&&c)return require(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u=undefined,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
@@ -3833,6 +3863,36 @@ if (true) {
 
 /***/ 808:
 /***/ ((module) => {
+
+/**
+ * Credits:
+ *
+ * lib-font
+ * https://github.com/Pomax/lib-font
+ * https://github.com/Pomax/lib-font/blob/master/lib/unbrotli.js
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 pomax@nihongoresources.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /* eslint eslint-comments/no-unlimited-disable: 0 */
 /* eslint-disable */
@@ -18981,6 +19041,36 @@ var unbrotli_default = /*#__PURE__*/__webpack_require__.n(unbrotli);
 var inflate = __webpack_require__(1497);
 var inflate_default = /*#__PURE__*/__webpack_require__.n(inflate);
 ;// CONCATENATED MODULE: ./packages/edit-site/lib/lib-font.browser.js
+/**
+ * Credits:
+ *
+ * lib-font
+ * https://github.com/Pomax/lib-font
+ * https://github.com/Pomax/lib-font/blob/master/lib-font.browser.js
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 pomax@nihongoresources.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /* eslint eslint-comments/no-unlimited-disable: 0 */
 /* eslint-disable */
 // import pako from 'pako';
