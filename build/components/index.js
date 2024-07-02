570e2c79197fff7bef7529532cc80c14ed49082e
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2024-07-02 19:45:25 +1000

    UnitControl: Fix an issue where keyboard shortcuts unintentionally shift focus on Windows OS (#62988)
    * UnitControl: Fix an issue where keyboard shortcuts unintentionally shift focus on Windows OS
    
    * Update changelog
    
    * Update comment
    
    Unlinked contributors: webexpr-dhenriet.
    
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: tyxla <tyxla@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/a19348b0fc25973b694b20556890b3fc07e188b7

--- build/components/index.js
+++ build/components/index.js
@@ -40252,10 +40252,10 @@ function UnforwardedUnitControl(unitControlProps, forwardedRef) {
   if (!disableUnits && isUnitSelectTabbable && units.length) {
     handleOnKeyDown = event => {
       props.onKeyDown?.(event);
-      // Unless the meta key was pressed (to avoid interfering with
-      // shortcuts, e.g. pastes), moves focus to the unit select if a key
+      // Unless the meta or ctrl key was pressed (to avoid interfering with
+      // shortcuts, e.g. pastes), move focus to the unit select if a key
       // matches the first character of a unit.
-      if (!event.metaKey && reFirstCharacterOfUnits.test(event.key)) {
+      if (!event.metaKey && !event.ctrlKey && reFirstCharacterOfUnits.test(event.key)) {
         refInputSuffix.current?.focus();
       }
     };
