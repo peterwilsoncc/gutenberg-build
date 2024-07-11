057bc9ec34b67200bef1f0382632160fde943322
Luis Herranz <luisherranz@gmail.com>
2024-07-11 10:24:35 +1000

    Remove unnecessary variables (#63290)
    Co-authored-by: luisherranz <luisherranz@git.wordpress.org>
    Co-authored-by: madhusudhand <madhudollu@git.wordpress.org>
    Co-authored-by: cbravobernal <cbravobernal@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/71134165868298fc15e22896d0c28b41b3755ff7

--- build/interactivity/image.js
+++ build/interactivity/image.js
@@ -48,20 +48,6 @@ let isTouching = false;
  * @type {number}
  */
 let lastTouchTime = 0;
-
-/**
- * Stores the image reference of the currently opened lightbox.
- *
- * @type {HTMLElement}
- */
-let imageRef;
-
-/**
- * Stores the button reference of the currently opened lightbox.
- *
- * @type {HTMLElement}
- */
-let buttonRef;
 const {
   state,
   actions,
@@ -101,8 +87,6 @@ const {
 
       // Moves the information of the expaned image to the state.
       ctx.currentSrc = ctx.imageRef.currentSrc;
-      imageRef = ctx.imageRef;
-      buttonRef = ctx.buttonRef;
       state.currentImage = ctx;
       state.overlayEnabled = true;
 
@@ -120,14 +104,12 @@ const {
           // Delays before changing the focus. Otherwise the focus ring will
           // appear on Firefox before the image has finished animating, which
           // looks broken.
-          buttonRef.focus({
+          state.currentImage.buttonRef.focus({
             preventScroll: true
           });
 
           // Resets the current image to mark the overlay as closed.
           state.currentImage = {};
-          imageRef = null;
-          buttonRef = null;
         }, 450);
 
         // Starts the overlay closing animation. The showClosingAnimation
@@ -195,7 +177,7 @@ const {
   },
   callbacks: {
     setOverlayStyles() {
-      if (!imageRef) {
+      if (!state.currentImage.imageRef) {
         return;
       }
       let {
@@ -203,11 +185,11 @@ const {
         naturalHeight,
         offsetWidth: originalWidth,
         offsetHeight: originalHeight
-      } = imageRef;
+      } = state.currentImage.imageRef;
       let {
         x: screenPosX,
         y: screenPosY
-      } = imageRef.getBoundingClientRect();
+      } = state.currentImage.imageRef.getBoundingClientRect();
 
       // Natural ratio of the image clicked to open the lightbox.
       const naturalRatio = naturalWidth / naturalHeight;
