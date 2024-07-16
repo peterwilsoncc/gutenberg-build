8fb98f086ff338c27481e33620c1b8e98222c446
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2024-07-16 15:22:19 +1000

    Global Styles: Disable "Reset styles" button when there are no changes (#63562)
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/b346c8e08584c8a5e528931c5f0387ca1c671634

--- build/block-editor/index.js
+++ build/block-editor/index.js
@@ -17351,9 +17351,13 @@ const EMPTY_CONFIG = {
 const VALID_SETTINGS = ['appearanceTools', 'useRootPaddingAwareAlignments', 'background.backgroundImage', 'background.backgroundRepeat', 'background.backgroundSize', 'background.backgroundPosition', 'border.color', 'border.radius', 'border.style', 'border.width', 'shadow.presets', 'shadow.defaultPresets', 'color.background', 'color.button', 'color.caption', 'color.custom', 'color.customDuotone', 'color.customGradient', 'color.defaultDuotone', 'color.defaultGradients', 'color.defaultPalette', 'color.duotone', 'color.gradients', 'color.heading', 'color.link', 'color.palette', 'color.text', 'custom', 'dimensions.aspectRatio', 'dimensions.minHeight', 'layout.contentSize', 'layout.definitions', 'layout.wideSize', 'lightbox.enabled', 'lightbox.allowEditing', 'position.fixed', 'position.sticky', 'spacing.customSpacingSize', 'spacing.defaultSpacingSizes', 'spacing.spacingSizes', 'spacing.spacingScale', 'spacing.blockGap', 'spacing.margin', 'spacing.padding', 'spacing.units', 'typography.fluid', 'typography.customFontSize', 'typography.defaultFontSizes', 'typography.dropCap', 'typography.fontFamilies', 'typography.fontSizes', 'typography.fontStyle', 'typography.fontWeight', 'typography.letterSpacing', 'typography.lineHeight', 'typography.textAlign', 'typography.textColumns', 'typography.textDecoration', 'typography.textTransform', 'typography.writingMode'];
 const useGlobalStylesReset = () => {
   const {
-    user: config,
+    user,
     setUserConfig
   } = (0,external_wp_element_namespaceObject.useContext)(GlobalStylesContext);
+  const config = {
+    settings: user.settings,
+    styles: user.styles
+  };
   const canReset = !!config && !es6_default()(config, EMPTY_CONFIG);
   return [canReset, (0,external_wp_element_namespaceObject.useCallback)(() => setUserConfig(EMPTY_CONFIG), [setUserConfig])];
 };
