4b80f479778a0a5c69c6efc669f64bd1d00431e8
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2024-09-18 17:13:05 +1000

    Block Bindings: Improve getRegisteredPostMeta resolver (#65450)
    * Do nothing when request fails
    
    * Update `State.registeredPostMeta`
    
    Co-authored-by: SantosGuillamot <santosguillamot@git.wordpress.org>
    Co-authored-by: gziolo <gziolo@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/e40612b7d642e312c1dee42866d295e3347669ea

--- build/core-data/index.js
+++ build/core-data/index.js
@@ -23464,18 +23464,22 @@ const resolvers_getRegisteredPostMeta = postType => async ({
   dispatch,
   resolveSelect
 }) => {
+  let options;
   try {
     const {
       rest_namespace: restNamespace = 'wp/v2',
       rest_base: restBase
     } = (await resolveSelect.getPostType(postType)) || {};
-    const options = await external_wp_apiFetch_default()({
+    options = await external_wp_apiFetch_default()({
       path: `${restNamespace}/${restBase}/?context=edit`,
       method: 'OPTIONS'
     });
+  } catch (error) {
+    // Do nothing if the request comes back with an API error.
+    return;
+  }
+  if (options) {
     dispatch.receiveRegisteredPostMeta(postType, options?.schema?.properties?.meta?.properties);
-  } catch {
-    dispatch.receiveRegisteredPostMeta(postType, false);
   }
 };
 
