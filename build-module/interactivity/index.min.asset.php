5607c06034115a635bd4af148bec831a1d47986e
David Arenas <david.arenas@automattic.com>
2024-09-19 10:35:01 +1000

    Interactivity API: Add `getServerState()` and `getServerContext()` (#65151)
    * Expose client and server context from provider
    
    * Create `getServerContext`
    
    * Add simple test for server context
    
    * Implement `getServerState`
    
    * Add tests for read-only state proxies
    
    * Add e2e tests for `getServerState()`
    
    * Avoid PHPCS UndefinedVariable error
    
    * Add e2e tests for `getServerContext` WIP
    
    * Finish e2e tests for `getServerContext()`
    
    * Update `getServerState()` tests
    
    * Revert "Add simple test for server context"
    
    This reverts commit 7e6f530a72dd9117946dd7e7fa9ab5c29f01d13d.
    
    * Update TSDocs
    
    Co-authored-by: DAreRodz <darerodz@git.wordpress.org>
    Co-authored-by: luisherranz <luisherranz@git.wordpress.org>
    Co-authored-by: sethrubenstein <smrubenstein@git.wordpress.org>
    Co-authored-by: michalczaplinski <czapla@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/fa1a0d6334260d3a4cc986bed848fb62fa7734ce

--- build-module/interactivity/index.min.asset.php
+++ build-module/interactivity/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '282416d8d6a4bb77cb09', 'type' => 'module');
+<?php return array('dependencies' => array(), 'version' => '5f35b6d3bd962e6e82b5', 'type' => 'module');
