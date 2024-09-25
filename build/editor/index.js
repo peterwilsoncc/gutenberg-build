35765ffc93e415466e8aad2c520dc1d963ce04b1
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2024-09-25 16:24:16 +1000

    Zoom out: Reset zoom out level when device type is changed (#65652)
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: draganescu <andraganescu@git.wordpress.org>
    Co-authored-by: AhmarZaidi <ahmarzaidi@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/5237aacfe0c183eb37e6cb857190295b236dcff2

--- build/editor/index.js
+++ build/editor/index.js
@@ -26157,6 +26157,7 @@ const tablet = /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(ext
 
 
 
+
 function PreviewDropdown({
   forceIsAutosaveable,
   disabled
@@ -26195,9 +26196,13 @@ function PreviewDropdown({
   const {
     __unstableSetEditorMode
   } = (0,external_wp_data_namespaceObject.useDispatch)(external_wp_blockEditor_namespaceObject.store);
+  const {
+    resetZoomLevel
+  } = unlock((0,external_wp_data_namespaceObject.useDispatch)(external_wp_blockEditor_namespaceObject.store));
   const handleDevicePreviewChange = newDeviceType => {
     setDeviceType(newDeviceType);
     __unstableSetEditorMode('edit');
+    resetZoomLevel();
   };
   const isMobile = (0,external_wp_compose_namespaceObject.useViewportMatch)('medium', '<');
   if (isMobile) {
