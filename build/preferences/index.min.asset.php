f00171b297a46ee6eb732c5fc5b6baee12ac386b
Daniel Richards <daniel.richards@automattic.com>
2024-10-03 14:42:05 +1000

    Fix cherry-pick issues - duplicated import and unused var
    Source: https://github.com/WordPress/gutenberg/commit/6e8539ba1e9a2fa3971a316ff9cda51e5153f78b

---
+++ build/preferences/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-private-apis'), 'version' => '01eeb2bdbd11f95bcf20');
