2e2eaf1c0ff2fc1192830b990ece1a2096a083b4
Daniel Richards <daniel.richards@automattic.com>
2024-10-03 11:38:39 +1000

    Top Toolbar: Show document bar when no block is selected even if block tools are expanded (#65839)
    * Top Toolbar: Show document bar when no block is selected even if block tools are expanded
    
    * Change variable name to hasBlockSelection
    
    ----
    
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    Co-authored-by: eatse21 <eatse@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/37e4dca8cbc0891b80dc2b070c2f734947dfb8d1

--- build/i18n/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => '5baa98e4345eccc97e24');
--- build/list-reusable-blocks/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '89a8ceb506e045dbe874');
