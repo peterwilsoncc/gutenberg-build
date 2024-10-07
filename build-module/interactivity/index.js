743924b005a19e1ecdb06b9bc97060e08c5e431e
Jon Surrell <sirreal@users.noreply.github.com>
2024-10-08 08:48:08 +1100

    Interactivity API: Allow "default" suffix values (#65815)
    Fix an issue where "default" could not be used as "suffix" values in
    Interactivity API directives, for example in a directive like:
    
        data-wp-class--default="…"
    
    ---
    
    Co-authored-by: sirreal <jonsurrell@git.wordpress.org>
    Co-authored-by: michalczaplinski <czapla@git.wordpress.org>
    Co-authored-by: gziolo <gziolo@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/3ee7c74df077955b96520c9737dd31192582fa23

--- build-module/interactivity/index.js
+++ build-module/interactivity/index.js
@@ -1390,6 +1390,12 @@ populateServerData(data);
 
 
 
+function isNonDefaultDirectiveSuffix(entry) {
+  return entry.suffix !== null;
+}
+function isDefaultDirectiveSuffix(entry) {
+  return entry.suffix === null;
+}
 // Main context.
 const context = (0,preact_module/* createContext */.kr)({
   client: {},
@@ -1408,7 +1414,7 @@ const directivePriorities = {};
  * directive(
  *   'alert', // Name without the `data-wp-` prefix.
  *   ( { directives: { alert }, element, evaluate } ) => {
- *     const defaultEntry = alert.find( entry => entry.suffix === 'default' );
+ *     const defaultEntry = alert.find( isDefaultDirectiveSuffix );
  *     element.props.onclick = () => { alert( evaluate( defaultEntry ) ); }
  *   }
  * )
@@ -1427,7 +1433,7 @@ const directivePriorities = {};
  * </div>
  * ```
  * Note that, in the previous example, the directive callback gets the path
- * value (`state.alert`) from the directive entry with suffix `default`. A
+ * value (`state.alert`) from the directive entry with suffix `null`. A
  * custom suffix can also be specified by appending `--` to the directive
  * attribute, followed by the suffix, like in the following HTML snippet:
  *
@@ -1603,9 +1609,7 @@ preact_module/* options */.YM.vnode = vnode => {
     const props = vnode.props;
     const directives = props.__directives;
     if (directives.key) {
-      vnode.key = directives.key.find(({
-        suffix
-      }) => suffix === 'default').value;
+      vnode.key = directives.key.find(isDefaultDirectiveSuffix).value;
     }
     delete props.__directives;
     const priorityLevels = getPriorityLevels(directives);
@@ -1700,9 +1704,7 @@ const getGlobalEventDirective = type => {
     directives,
     evaluate
   }) => {
-    directives[`on-${type}`].filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    directives[`on-${type}`].filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const eventName = entry.suffix.split('--', 1)[0];
       useInit(() => {
         const cb = event => evaluate(entry, event);
@@ -1725,9 +1727,7 @@ const getGlobalAsyncEventDirective = type => {
     directives,
     evaluate
   }) => {
-    directives[`on-async-${type}`].filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    directives[`on-async-${type}`].filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const eventName = entry.suffix.split('--', 1)[0];
       useInit(() => {
         const cb = async event => {
@@ -1757,9 +1757,7 @@ const getGlobalAsyncEventDirective = type => {
     const {
       Provider
     } = inheritedContext;
-    const defaultEntry = context.find(({
-      suffix
-    }) => suffix === 'default');
+    const defaultEntry = context.find(isDefaultDirectiveSuffix);
     const {
       client: inheritedClient,
       server: inheritedServer
@@ -1857,9 +1855,7 @@ const getGlobalAsyncEventDirective = type => {
     evaluate
   }) => {
     const events = new Map();
-    on.filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    on.filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const event = entry.suffix.split('--')[0];
       if (!events.has(event)) {
         events.set(event, new Set());
@@ -1895,9 +1891,7 @@ const getGlobalAsyncEventDirective = type => {
     evaluate
   }) => {
     const events = new Map();
-    onAsync.filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    onAsync.filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const event = entry.suffix.split('--')[0];
       if (!events.has(event)) {
         events.set(event, new Set());
@@ -1936,9 +1930,7 @@ const getGlobalAsyncEventDirective = type => {
     element,
     evaluate
   }) => {
-    classNames.filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    classNames.filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const className = entry.suffix;
       const result = evaluate(entry);
       const currentClass = element.props.class || '';
@@ -1971,9 +1963,7 @@ const getGlobalAsyncEventDirective = type => {
     element,
     evaluate
   }) => {
-    style.filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    style.filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const styleProp = entry.suffix;
       const result = evaluate(entry);
       element.props.style = element.props.style || {};
@@ -2008,9 +1998,7 @@ const getGlobalAsyncEventDirective = type => {
     element,
     evaluate
   }) => {
-    bind.filter(({
-      suffix
-    }) => suffix !== 'default').forEach(entry => {
+    bind.filter(isNonDefaultDirectiveSuffix).forEach(entry => {
       const attribute = entry.suffix;
       const result = evaluate(entry);
       element.props[attribute] = result;
@@ -2094,9 +2082,7 @@ const getGlobalAsyncEventDirective = type => {
     element,
     evaluate
   }) => {
-    const entry = text.find(({
-      suffix
-    }) => suffix === 'default');
+    const entry = text.find(isDefaultDirectiveSuffix);
     if (!entry) {
       element.props.children = null;
       return;
@@ -2138,12 +2124,11 @@ const getGlobalAsyncEventDirective = type => {
     const inheritedValue = q(inheritedContext);
     const [entry] = each;
     const {
-      namespace,
-      suffix
+      namespace
     } = entry;
     const list = evaluate(entry);
+    const itemProp = isNonDefaultDirectiveSuffix(entry) ? kebabToCamelCase(entry.suffix) : 'item';
     return list.map(item => {
-      const itemProp = suffix === 'default' ? 'item' : kebabToCamelCase(suffix);
       const itemContext = proxifyContext(proxifyState(namespace, {}), inheritedValue.client[namespace]);
       const mergedContext = {
         client: {
@@ -2314,11 +2299,11 @@ function toVdom(root) {
           return obj;
         }
         const prefix = directiveMatch[1] || '';
-        const suffix = directiveMatch[2] || 'default';
+        const suffix = directiveMatch[2] || null;
         obj[prefix] = obj[prefix] || [];
         obj[prefix].push({
           namespace: ns !== null && ns !== void 0 ? ns : currentNamespace(),
-          value,
+          value: value,
           suffix
         });
         return obj;
