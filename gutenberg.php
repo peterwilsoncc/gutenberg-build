d2587e68a5f8d5dc5e0bbcb25df26494450b4931
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2024-10-09 10:56:30 +1100

    Revert "Bump plugin version to 19.4.0"
    This reverts commit 43b613dc798a172000b218ec0b593653c2550fe9.
    
    Source: https://github.com/WordPress/gutenberg/commit/841bcc09d2b27989c8fd43043db24f63e9399ea2

--- gutenberg.php
+++ gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.5
  * Requires PHP: 7.2
- * Version: 19.4.0
+ * Version: 19.4.0-rc.1
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -14,7 +14,7 @@
 
 ### BEGIN AUTO-GENERATED DEFINES
 define( 'GUTENBERG_VERSION', '19.4.0' );
-define( 'GUTENBERG_GIT_COMMIT', '43b613dc798a172000b218ec0b593653c2550fe9' );
+define( 'GUTENBERG_GIT_COMMIT', '841bcc09d2b27989c8fd43043db24f63e9399ea2' );
 ### END AUTO-GENERATED DEFINES
 defined( 'GUTENBERG_MINIMUM_WP_VERSION' ) or define( 'GUTENBERG_MINIMUM_WP_VERSION', '6.5' );
 
