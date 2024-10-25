819118d023dec762a72a28cbabb88ca8acfac825
Mitchell Austin <mr.fye@oneandthesame.net>
2024-10-25 13:33:52 +1100

    Revise zoom layout shift fix (#66390)
    * Contain margins with BFC instead of border
    
    * Move to block canvas styles in the visual editor
    
    ---------
    
    Co-authored-by: Andrew Serong <14988353+andrewserong@users.noreply.github.com>
    Co-authored-by: stokesman <presstoke@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/268b0fe815bc8ad6861a9e3579e4bebf56fddc56

--- build/editor/index.js
+++ build/editor/index.js
@@ -28782,7 +28782,10 @@ function VisualEditor({
   !isZoomedOut;
   const iframeStyles = (0,external_wp_element_namespaceObject.useMemo)(() => {
     return [...(styles !== null && styles !== void 0 ? styles : []), {
-      css: `.is-root-container{display:flow-root;${
+      // Ensures margins of children are contained so that the body background paints behind them.
+      // Otherwise, the background of html (when zoomed out) would show there and appear broken. It’s
+      // important mostly for post-only views yet conceivably an issue in templated views too.
+      css: `:where(.block-editor-iframe__body){display:flow-root;}.is-root-container{display:flow-root;${
       // Some themes will have `min-height: 100vh` for the root container,
       // which isn't a requirement in auto resize mode.
       enableResizing ? 'min-height:0!important;' : ''}}`
