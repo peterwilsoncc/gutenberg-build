2d9156f363a347d05cdf3efc9e89741d2f592087
Ella <4710635+ellatrix@users.noreply.github.com>
2024-10-28 17:38:05 +1100

    Preload: fix multiple regressions around global styles (#66468)
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: aaronrobertshaw <aaronrobertshaw@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/60528475cf5a21785475a5e9c05c72ebf8ce4c2b

--- build/core-data/index.js
+++ build/core-data/index.js
@@ -23237,6 +23237,7 @@ const resolvers_experimentalGetCurrentThemeBaseGlobalStyles = () => async ({
   dispatch
 }) => {
   const currentTheme = await resolveSelect.getCurrentTheme();
+  // Please adjust the preloaded requests if this changes!
   const themeGlobalStyles = await external_wp_apiFetch_default()({
     path: `/wp/v2/global-styles/themes/${currentTheme.stylesheet}?context=view`
   });
@@ -23247,6 +23248,7 @@ const resolvers_experimentalGetCurrentThemeGlobalStylesVariations = () => async 
   dispatch
 }) => {
   const currentTheme = await resolveSelect.getCurrentTheme();
+  // Please adjust the preloaded requests if this changes!
   const variations = await external_wp_apiFetch_default()({
     path: `/wp/v2/global-styles/themes/${currentTheme.stylesheet}/variations?context=view`
   });
