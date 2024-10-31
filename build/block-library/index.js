be9ece6ca58d67e4767923cfe3561de3a0605977
Sarah Norris <1645628+mikachan@users.noreply.github.com>
2024-10-31 20:10:18 +1100

    Query Loop: Check for postTypeFromContext before using it (#66655)
    Co-authored-by: mikachan <mikachan@git.wordpress.org>
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/2f088e403b47f2380e05ec2448a5902af0cca289

--- build/block-library/index.js
+++ build/block-library/index.js
@@ -44405,7 +44405,7 @@ function PostTemplateEdit({
     }
     // When we preview Query Loop blocks we should prefer the current
     // block's postType, which is passed through block context.
-    const usedPostType = postTypeFromContext !== 'page' ? postTypeFromContext : previewPostType || postType;
+    const usedPostType = postTypeFromContext && postTypeFromContext !== 'page' ? postTypeFromContext : previewPostType || postType;
     return {
       posts: getEntityRecords('postType', usedPostType, {
         ...query,
