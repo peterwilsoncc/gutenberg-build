6d067d5ad71f82a975e84d7b75faa6a7666de0bf
Gutenberg Repository Automation <gutenberg@wordpress.org>
2024-11-01 13:20:18 +1100

    Bump plugin version to 19.6.0-rc.3
    Source: https://github.com/WordPress/gutenberg/commit/88b9804bd1abdde4ebc4815152077f0bf6b480f5

--- gutenberg.php
+++ gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.5
  * Requires PHP: 7.2
- * Version: 19.6.0-rc.2
+ * Version: 19.6.0-rc.3
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -14,7 +14,7 @@
 
 ### BEGIN AUTO-GENERATED DEFINES
 define( 'GUTENBERG_VERSION', '19.6.0' );
-define( 'GUTENBERG_GIT_COMMIT', '9b9a36749376f38ce3add6e3604effb7b948f4d7' );
+define( 'GUTENBERG_GIT_COMMIT', '88b9804bd1abdde4ebc4815152077f0bf6b480f5' );
 ### END AUTO-GENERATED DEFINES
 defined( 'GUTENBERG_MINIMUM_WP_VERSION' ) or define( 'GUTENBERG_MINIMUM_WP_VERSION', '6.5' );
 
