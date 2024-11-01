6aaf04be56b1301e177551d44ebf51651223edf2
arthur791004 <arthur.chu@automattic.com>
2024-11-01 13:08:38 +1100

    Post Editor: Set the default value of the editorTool to edit (#66636)
    * Post Editor: Set the default value of the editorTool to edit
    
    * Change the condition of _showEmptyBlockSideInserter
    
    * Set the default value of the editorTool to "edit" for both post editor and site editor
    
    Source: https://github.com/WordPress/gutenberg/commit/9b9a36749376f38ce3add6e3604effb7b948f4d7

--- build/edit-post/index.js
+++ build/edit-post/index.js
@@ -3453,6 +3453,7 @@ function initializeEditor(id, postType, postId, settings, initialEdits) {
   (0,external_wp_data_namespaceObject.dispatch)(external_wp_preferences_namespaceObject.store).setDefaults('core', {
     allowRightClickOverrides: true,
     editorMode: 'visual',
+    editorTool: 'edit',
     fixedToolbar: false,
     hiddenBlockTypes: [],
     inactivePanels: [],
