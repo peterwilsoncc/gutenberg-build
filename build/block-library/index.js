631f837c01d2c82bbab74e7dadf1502a851b4b7c
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2024-11-04 07:03:34 +1100

    Block Inspector: Restore bottm margin for RadioControl (#66688)
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: up1512001 <up1512001@git.wordpress.org>
    Co-authored-by: aaronrobertshaw <aaronrobertshaw@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/9631601d078776347dc00c814d08ed8de06545b6

--- build/block-library/index.js
+++ build/block-library/index.js
@@ -28190,7 +28190,6 @@ function LatestPostsEdit({
           displayPostContent: value
         })
       }), displayPostContent && /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.RadioControl, {
-        className: "wp-block-latest-posts__post-content-radio",
         label: (0,external_wp_i18n_namespaceObject.__)('Show'),
         selected: displayPostContentRadio,
         options: [{
