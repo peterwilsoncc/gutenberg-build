fe71b1c6c435497660b778f39e80894f1ac12620
Weston Ruter <westonruter@google.com>
2024-11-06 15:46:36 +1100

    Delay block hydration to allow stores to initialize (#66772)
    Co-authored-by: westonruter <westonruter@git.wordpress.org>
    Co-authored-by: michalczaplinski <czapla@git.wordpress.org>
    Co-authored-by: annezazu <annezazu@git.wordpress.org>
    Co-authored-by: artemiomorales <artemiosans@git.wordpress.org>
    Co-authored-by: cbravobernal <cbravobernal@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/f4b013f830d526f6d361627dd73642ac6931d8a0

--- build-module/interactivity/index.js
+++ build-module/interactivity/index.js
@@ -253,7 +253,7 @@ const afterNextFrame = callback => {
 /**
  * Returns a promise that resolves after yielding to main.
  *
- * @return Promise
+ * @return Promise<void>
  */
 const splitTask = typeof window.scheduler?.yield === 'function' ? window.scheduler.yield.bind(window.scheduler) : () => {
   return new Promise(resolve => {
@@ -2394,6 +2394,17 @@ const initialVdom = new WeakMap();
 // Initialize the router with the initial DOM.
 const init = async () => {
   const nodes = document.querySelectorAll(`[data-${directivePrefix}-interactive]`);
+
+  /*
+   * This `await` with setTimeout is required to apparently ensure that the interactive blocks have their stores
+   * fully initialized prior to hydrating the blocks. If this is not present, then an error occurs, for example:
+   * > view.js:46 Uncaught (in promise) ReferenceError: Cannot access 'state' before initialization
+   * This occurs when splitTask() is implemented with scheduler.yield() as opposed to setTimeout(), as with the former
+   * split tasks are added to the front of the task queue whereas with the latter they are added to the end of the queue.
+   */
+  await new Promise(resolve => {
+    setTimeout(resolve, 0);
+  });
   for (const node of nodes) {
     if (!hydratedIslands.has(node)) {
       await splitTask();
