peterwilsoncc/gutenberg-build

commit 19dcbbfed906a81e0fe7261563fa74f3ef732738

TypeScript: Convert factory utils in data package to TS (#67667)
* TypeScript: Convert factory utils in data package to TS

* Fix docgen build error

* Update docs

* Revert and fix TS error

* Extract and improve signature

Source: https://github.com/WordPress/gutenberg/commit/6b8e47fbd17c0710a4e55d4104dce185fdfac53b

build/core-data/index.js
@@ -4293,7 +4293,7 @@ const getEntityRecordsPermissions = (0,external_wp_data_namespaceObject.createRe
  * @return The entity record permissions.
  */
 function getEntityRecordPermissions(state, kind, name, id) {
-  return getEntityRecordsPermissions(state, kind, name, id)[0];
+  return getEntityRecordsPermissions(state, kind, name, [id])[0];
 }
 
 /**

build/core-data/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-private-apis', 'wp-rich-text', 'wp-url', 'wp-warning'), 'version' => 'aeebc905cb89bece58ac');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-private-apis', 'wp-rich-text', 'wp-url', 'wp-warning'), 'version' => 'e5b178a167338f943b9a');

gutenberg.php
@@ -14,7 +14,7 @@
 
 ### BEGIN AUTO-GENERATED DEFINES
 define( 'GUTENBERG_VERSION', '19.8.0' );
-define( 'GUTENBERG_GIT_COMMIT', 'e85937ff6868a4b35856845755636c39dbbc555a' );
+define( 'GUTENBERG_GIT_COMMIT', '6b8e47fbd17c0710a4e55d4104dce185fdfac53b' );
 ### END AUTO-GENERATED DEFINES
 defined( 'GUTENBERG_MINIMUM_WP_VERSION' ) or define( 'GUTENBERG_MINIMUM_WP_VERSION', '6.6' );
 

build/data/index.js
@@ -1002,6 +1002,10 @@ function combine_reducers_combineReducers(reducers) {
 
 ;// ./packages/data/build-module/factory.js
 /**
+ * Internal dependencies
+ */
+
+/**
  * Creates a selector function that takes additional curried argument with the
  * registry `select` function. While a regular selector has signature
  * ```js
@@ -1036,10 +1040,10 @@ function combine_reducers_combineReducers(reducers) {
  * registry as argument. The registry binding happens automatically when registering the selector
  * with a store.
  *
- * @param {Function} registrySelector Function receiving a registry `select`
- *                                    function and returning a state selector.
+ * @param registrySelector Function receiving a registry `select`
+ *                         function and returning a state selector.
  *
- * @return {Function} Registry selector that can be registered with a store.
+ * @return Registry selector that can be registered with a store.
  */
 function createRegistrySelector(registrySelector) {
   const selectorsByRegistry = new WeakMap();
@@ -1063,8 +1067,6 @@ function createRegistrySelector(registrySelector) {
    * Flag indicating that the selector is a registry selector that needs the correct registry
    * reference to be assigned to `selector.registry` to make it work correctly.
    * be mapped as a registry selector.
-   *
-   * @type {boolean}
    */
   wrappedSelector.isRegistrySelector = true;
   return wrappedSelector;
@@ -1086,9 +1088,9 @@ function createRegistrySelector(registrySelector) {
  * When registering a control created with `createRegistryControl` with a store, the store
  * knows which calling convention to use when executing the control.
  *
- * @param {Function} registryControl Function receiving a registry object and returning a control.
+ * @param registryControl Function receiving a registry object and returning a control.
  *
- * @return {Function} Registry control that can be registered with a store.
+ * @return Registry control that can be registered with a store.
  */
 function createRegistryControl(registryControl) {
   registryControl.isRegistryControl = true;

commit 3453eefd107fea634d7a30f5e1096ef2af7f4d11

Fix re-renders caused by `getEntityRecordsPermissions` after #67667 (#67770)
* Update getEntityRecordsPermissions signature to allow string ids
* Fix regression caused in #67667

Co-authored-by: manzoorwanijk <manzoorwanijk@git.wordpress.org>
Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>

Source: https://github.com/WordPress/gutenberg/commit/4867dfc6bb9286865ae19e332b06a29b35ce7ab1

build/core-data/index.js
@@ -4293,7 +4293,7 @@ const getEntityRecordsPermissions = (0,external_wp_data_namespaceObject.createRe
  * @return The entity record permissions.
  */
 function getEntityRecordPermissions(state, kind, name, id) {
-  return getEntityRecordsPermissions(state, kind, name, [id])[0];
+  return getEntityRecordsPermissions(state, kind, name, id)[0];
 }
 
 /**

build/core-data/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-private-apis', 'wp-rich-text', 'wp-url', 'wp-warning'), 'version' => 'e5b178a167338f943b9a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-private-apis', 'wp-rich-text', 'wp-url', 'wp-warning'), 'version' => 'aeebc905cb89bece58ac');

gutenberg.php
@@ -14,7 +14,7 @@
 
 ### BEGIN AUTO-GENERATED DEFINES
 define( 'GUTENBERG_VERSION', '19.8.0' );
-define( 'GUTENBERG_GIT_COMMIT', '7159a7857fb9106c4ecaa299220de91f0fff23c8' );
+define( 'GUTENBERG_GIT_COMMIT', '4867dfc6bb9286865ae19e332b06a29b35ce7ab1' );
 ### END AUTO-GENERATED DEFINES
 defined( 'GUTENBERG_MINIMUM_WP_VERSION' ) or define( 'GUTENBERG_MINIMUM_WP_VERSION', '6.6' );
 