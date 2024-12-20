31b2e5ed0d7a333843805c605b6961c91f0ded7a
Sainath Poojary <53347682+SainathPoojary@users.noreply.github.com>
2024-12-20 11:55:57 +1100

    Storybook: Add PlainText Storybook stories (#67341)
    * Storybook: Add PlainText Storybook stories
    
    - Add Default, LongText, and WithClassName stories
    - Configure meta for PlainText component
    
    * Refactor: `PlainText` stories and removed useEffect
    
    * Refactor: Remove comment from story
    
    * Storybook: improve `PlainText` JSDoc and storybook descriptions
    
    - Enhance prop descriptions
    - Fix import order in storybook.
    
    * Storybook: Unify value and onChange prop descriptions
    
    Co-authored-by: SainathPoojary <sainathpoojary@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/ad073b08924d1c7c1c7d673bab951a1b05023257

--- build/block-editor/index.js
+++ build/block-editor/index.js
@@ -73194,7 +73194,41 @@ EditableText.Content = ({
 
 
 /**
+ * Render an auto-growing textarea allow users to fill any textual content.
+ *
  * @see https://github.com/WordPress/gutenberg/blob/HEAD/packages/block-editor/src/components/plain-text/README.md
+ *
+ * @example
+ * ```jsx
+ * import { registerBlockType } from '@wordpress/blocks';
+ * import { PlainText } from '@wordpress/block-editor';
+ *
+ * registerBlockType( 'my-plugin/example-block', {
+ *   // ...
+ *
+ *   attributes: {
+ *     content: {
+ *       type: 'string',
+ *     },
+ *   },
+ *
+ *   edit( { className, attributes, setAttributes } ) {
+ *     return (
+ *       <PlainText
+ *         className={ className }
+ *         value={ attributes.content }
+ *         onChange={ ( content ) => setAttributes( { content } ) }
+ *       />
+ *     );
+ *   },
+ * } );
+ * ````
+ *
+ * @param {Object}   props          Component props.
+ * @param {string}   props.value    String value of the textarea.
+ * @param {Function} props.onChange Function called when the text value changes.
+ * @param {Object}   [props.ref]    The component forwards the `ref` property to the `TextareaAutosize` component.
+ * @return {Element} Plain text component
  */
 
 const PlainText = (0,external_wp_element_namespaceObject.forwardRef)(({
