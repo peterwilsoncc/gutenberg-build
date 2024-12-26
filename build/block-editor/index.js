884e9c07a558f3d58cf2f9c0f2462979ea1adbe9
George Mamadashvili <georgemamadashvili@gmail.com>
2024-12-26 11:11:51 +1100

    Block Editor: Fix the 'Reset all' bug for the 'ResolutionTool' component (#68296)
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/c0329d9bc97454dd06589e5921b46659923af028

--- build/block-editor/index.js
+++ build/block-editor/index.js
@@ -79289,7 +79289,8 @@ function ResolutionTool({
   onChange,
   options = DEFAULT_SIZE_OPTIONS,
   defaultValue = DEFAULT_SIZE_OPTIONS[0].value,
-  isShownByDefault = true
+  isShownByDefault = true,
+  resetAllFilter
 }) {
   const displayValue = value !== null && value !== void 0 ? value : defaultValue;
   return /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.__experimentalToolsPanelItem, {
@@ -79298,6 +79299,7 @@ function ResolutionTool({
     onDeselect: () => onChange(defaultValue),
     isShownByDefault: isShownByDefault,
     panelId: panelId,
+    resetAllFilter: resetAllFilter,
     children: /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.SelectControl, {
       __nextHasNoMarginBottom: true,
       label: (0,external_wp_i18n_namespaceObject.__)('Resolution'),
