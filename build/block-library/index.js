98ee5f90c30ea405f4179f15ab0af9a25eab19f4
George Mamadashvili <georgemamadashvili@gmail.com>
2025-01-13 08:34:10 +1100

    Site Logo: Prevent uploading multiple images via drag and drop (#68618)
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/d039f916823698848c7732f93c4b4d0dd860be90

--- build/block-library/index.js
+++ build/block-library/index.js
@@ -55252,6 +55252,10 @@ function LogoEdit({
     setTemporaryURL();
   };
   const onFilesDrop = filesList => {
+    if (filesList?.length > 1) {
+      onUploadError((0,external_wp_i18n_namespaceObject.__)('Only one image can be used as a site logo.'));
+      return;
+    }
     getSettings().mediaUpload({
       allowedTypes: site_logo_edit_ALLOWED_MEDIA_TYPES,
       filesList,
