9ae85146d6ab50990d2fcddb4cfd234ebb97ea6a
George Mamadashvili <georgemamadashvili@gmail.com>
2025-01-17 16:24:05 +1100

    Editor: Update post-type support key for new default rendering mode (#68745)
    * Editor: Update post-type support key for new default rendering mode
    * Add backport changelog entry
    
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: fabiankaegy <fabiankaegy@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/12709bd7723a3cc71ef3592fd15699ab9bdbbbc6

--- build/editor/index.js
+++ build/editor/index.js
@@ -14861,7 +14861,7 @@ const ExperimentalEditorProvider = with_registry_provider(({
     } = select(external_wp_coreData_namespaceObject.store);
     const postTypeSupports = getPostType(post.type)?.supports;
     const hasLoadedPostObject = hasFinishedResolution('getPostType', [post.type]);
-    const _defaultMode = Array.isArray(postTypeSupports?.editor) ? postTypeSupports.editor.find(features => 'default_mode' in features)?.default_mode : undefined;
+    const _defaultMode = Array.isArray(postTypeSupports?.editor) ? postTypeSupports.editor.find(features => 'default-mode' in features)?.['default-mode'] : undefined;
     const hasDefaultMode = RENDERING_MODES.includes(_defaultMode);
     return {
       editorSettings: getEditorSettings(),
