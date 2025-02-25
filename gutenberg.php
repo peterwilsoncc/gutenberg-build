b007ac72fc0883c9be084b25925eda947d1eece5
Bernie Reiter <96308+ockham@users.noreply.github.com>
2025-02-25 21:00:58 +1100

    Template Part block: Guard against `get_block_file_template` returning `null` (#69309)
    Co-authored-by: ockham <bernhard-reiter@git.wordpress.org>
    Co-authored-by: paulopmt1 <paulopmt1@git.wordpress.org>
    Co-authored-by: josephscott <josephscott@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/9adf74a59243f90f818bcddc0e3f45a6d5a66e07

--- gutenberg.php
+++ gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.6
  * Requires PHP: 7.2
- * Version: 20.0.2
+ * Version: 20.0.3
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '20.0.2' );
-define( 'GUTENBERG_GIT_COMMIT', '64f6a1b606c48d611adf06173e74bcdf22c8d93c' );
+define( 'GUTENBERG_VERSION', '20.0.3' );
+define( 'GUTENBERG_GIT_COMMIT', '887b66cdd7be926ea7dd26ed4be0748e741b7c4d' );
 ### END AUTO-GENERATED DEFINES
 defined( 'GUTENBERG_MINIMUM_WP_VERSION' ) or define( 'GUTENBERG_MINIMUM_WP_VERSION', '6.6' );
 
