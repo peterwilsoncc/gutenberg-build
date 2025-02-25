ac869159585e5619f6658f725eae79388e2e7912
Andrea Fercia <a.fercia@gmail.com>
2025-02-25 14:48:30 +1100

    Swap fullscreen mode snackbar notice message. (#69305)
    Co-authored-by: afercia <afercia@git.wordpress.org>
    Co-authored-by: carolinan <poena@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/db7153125f6dcbd7135ac12af3adc6e56f96109f

--- build/edit-post/index.js
+++ build/edit-post/index.js
@@ -1065,7 +1065,7 @@ const toggleFullscreenMode = () => ({
 }) => {
   const isFullscreen = registry.select(external_wp_preferences_namespaceObject.store).get('core/edit-post', 'fullscreenMode');
   registry.dispatch(external_wp_preferences_namespaceObject.store).toggle('core/edit-post', 'fullscreenMode');
-  registry.dispatch(external_wp_notices_namespaceObject.store).createInfoNotice(isFullscreen ? (0,external_wp_i18n_namespaceObject.__)('Fullscreen mode activated.') : (0,external_wp_i18n_namespaceObject.__)('Fullscreen mode deactivated.'), {
+  registry.dispatch(external_wp_notices_namespaceObject.store).createInfoNotice(isFullscreen ? (0,external_wp_i18n_namespaceObject.__)('Fullscreen mode deactivated.') : (0,external_wp_i18n_namespaceObject.__)('Fullscreen mode activated.'), {
     id: 'core/edit-post/toggle-fullscreen-mode/notice',
     type: 'snackbar',
     actions: [{
