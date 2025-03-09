f57e62c5086449add4e3f7514dbf0834ffa67071
Mitchell Austin <mr.fye@oneandthesame.net>
2025-03-10 08:19:52 +1100

    Fix error from popover scroll hook (#69479)
    Co-authored-by: stokesman <presstoke@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/2141278d2bada98f46e07ce54670aca71c5b74f0

--- build/block-editor/index.js
+++ build/block-editor/index.js
@@ -32713,7 +32713,7 @@ const scrollContainerCache = new WeakMap();
  * @param {Object} contentRef
  */
 function usePopoverScroll(contentRef) {
-  return (0,external_wp_compose_namespaceObject.useRefEffect)(node => {
+  const effect = (0,external_wp_compose_namespaceObject.useRefEffect)(node => {
     function onWheel(event) {
       const {
         deltaX,
@@ -32737,6 +32737,7 @@ function usePopoverScroll(contentRef) {
       node.removeEventListener('wheel', onWheel, options);
     };
   }, [contentRef]);
+  return contentRef ? effect : null;
 }
 /* harmony default export */ const use_popover_scroll = (usePopoverScroll);
 
