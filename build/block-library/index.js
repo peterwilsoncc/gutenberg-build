da818f578730aca64cf35c97f6e45f4f1c34da17
Hilay Trivedi <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-03-10 09:29:19 +1100

    Query Total: Update the block color support settings (#69500)
    * Fix: Added text color support along with background color support.
    
    * Remove redudant field definition
    
    ---------
    
    Co-authored-by: HILAYTRIVEDI <hilayt24@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: sarthaknagoshe2002 <sarthaknagoshe2002@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: code-with-mehedi <mehedi_csit@git.wordpress.org>
    Co-authored-by: carolinan <poena@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/95d6620b36313e0a606c16867440ba9ae597a775

--- build/block-library/index.js
+++ build/block-library/index.js
@@ -52098,9 +52098,9 @@ const query_total_metadata = {
     },
     color: {
       gradients: true,
-      text: true,
       __experimentalDefaultControls: {
-        background: true
+        background: true,
+        text: true
       }
     },
     typography: {
