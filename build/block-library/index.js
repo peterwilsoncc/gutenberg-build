7d23196cfa31979e4a53261606014d4200946b10
George Mamadashvili <georgemamadashvili@gmail.com>
2025-04-29 14:01:26 +1000

    Image: Avoid stale URL when reselecting the same image from the library (#69985)
    Unlinked contributors: T4ngml.
    
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/145ff0615b26f58214b18f363e198898af7e8eb5

--- build/block-library/index.js
+++ build/block-library/index.js
@@ -27769,12 +27769,6 @@ function ImageEdit({
       additionalAttributes = {
         sizeSlug: newSize
       };
-    } else {
-      // Keep the same url when selecting the same file, so "Resolution"
-      // option is not changed.
-      additionalAttributes = {
-        url
-      };
     }
 
     // Check if default link setting should be used.
