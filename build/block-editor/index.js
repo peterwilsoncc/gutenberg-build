107be4c546cf5d1a61c141092faeef387f54e56b
Mayank Tripathi <70465598+Mayank-Tripathi32@users.noreply.github.com>
2025-05-05 12:43:23 +1000

    MediaPlaceholder: Fix Regression with media URL input type to allow a local URL path (#70043)
    Co-authored-by: Mayank-Tripathi32 <mayanktripathi32@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: skierpage <skierpage@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/bb75eeabb982ee282426610f0a161cc1954e8ac5

--- build/block-editor/index.js
+++ build/block-editor/index.js
@@ -72421,7 +72421,8 @@ const InsertFromURLPopover = ({
     children: /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.__experimentalInputControl, {
       __next40pxDefaultSize: true,
       label: (0,external_wp_i18n_namespaceObject.__)('URL'),
-      type: "url",
+      type: "text" // Use text instead of URL to allow relative paths (e.g., /image/image.jpg)
+      ,
       hideLabelFromVision: true,
       placeholder: (0,external_wp_i18n_namespaceObject.__)('Paste or type URL'),
       onChange: onChange,
