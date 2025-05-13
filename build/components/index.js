eb1fcbcfacdef91550d73718a30c2a0b2df5d889
Yogesh Bhutkar <yogesh.bhutkar@rtcamp.com>
2025-05-13 21:42:35 +1000

    `withIgnoreIMEEvents`: Update documentation for clarity (#70098)
    Co-authored-by: yogeshbhutkar <yogeshbhutkar@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/3a5d9f07e4cc88a0e84b7ed5d1f5cbe950e9623c

--- build/components/index.js
+++ build/components/index.js
@@ -31366,18 +31366,18 @@ function useInputControlStateReducer(stateReducer = initialStateReducer, initial
 
 ;// ./packages/components/build-module/utils/with-ignore-ime-events.js
 /**
- * A higher-order function that wraps a keydown event handler to ensure it is not an IME event.
+ * A higher-order function that wraps a keyboard event handler to ensure it is not an IME event.
  *
  * In CJK languages, an IME (Input Method Editor) is used to input complex characters.
- * During an IME composition, keydown events (e.g. Enter or Escape) can be fired
+ * During an IME composition, keyboard events (e.g. Enter or Escape) can be fired
  * which are intended to control the IME and not the application.
  * These events should be ignored by any application logic.
  *
- * @param keydownHandler The keydown event handler to execute after ensuring it was not an IME event.
+ * @param handler The keyboard event handler to execute after ensuring it was not an IME event.
  *
  * @return A wrapped version of the given event handler that ignores IME events.
  */
-function withIgnoreIMEEvents(keydownHandler) {
+function withIgnoreIMEEvents(handler) {
   return event => {
     const {
       isComposing
@@ -31389,7 +31389,7 @@ function withIgnoreIMEEvents(keydownHandler) {
     event.keyCode === 229) {
       return;
     }
-    keydownHandler(event);
+    handler(event);
   };
 }
 
