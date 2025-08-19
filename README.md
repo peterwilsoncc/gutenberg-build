65d28c2a86406cd34c2a63925089fb7e390ca59f
Mike Jolley <mike.jolley@me.com>
2025-08-19 18:00:51 +1000

    DataForm: panel layout can open as `dropdown` or `modal` (#71212)
    Co-authored-by: mikejolley <mikejolley@git.wordpress.org>
    Co-authored-by: jameskoster <jameskoster@git.wordpress.org>
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/4ba01f0a26161a5fa57e6a52b21161a8c11f99a1

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`6172a890a0`](https://github.com/WordPress/gutenberg/commit/6172a890a0e99999514bb22ca9a503fe8d86d725), [Browse files](https://github.com/WordPress/gutenberg/tree/6172a890a0e99999514bb22ca9a503fe8d86d725)
+* Commit: [WordPress/Gutenberg@`4ba01f0a26`](https://github.com/WordPress/gutenberg/commit/4ba01f0a26161a5fa57e6a52b21161a8c11f99a1), [Browse files](https://github.com/WordPress/gutenberg/tree/4ba01f0a26161a5fa57e6a52b21161a8c11f99a1)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/6172a890a0e99999514bb22ca9a503fe8d86d725/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/4ba01f0a26161a5fa57e6a52b21161a8c11f99a1/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/edit-site/index.js
+++ build/edit-site/index.js
@@ -51662,10 +51662,11 @@ function normalizeLayout(layout) {
       labelPosition: (_layout$labelPosition = layout?.labelPosition) !== null && _layout$labelPosition !== void 0 ? _layout$labelPosition : 'top'
     };
   } else if (layout?.type === 'panel') {
-    var _layout$labelPosition2;
+    var _layout$labelPosition2, _layout$openAs;
     normalizedLayout = {
       type: 'panel',
-      labelPosition: (_layout$labelPosition2 = layout?.labelPosition) !== null && _layout$labelPosition2 !== void 0 ? _layout$labelPosition2 : 'side'
+      labelPosition: (_layout$labelPosition2 = layout?.labelPosition) !== null && _layout$labelPosition2 !== void 0 ? _layout$labelPosition2 : 'side',
+      openAs: (_layout$openAs = layout?.openAs) !== null && _layout$openAs !== void 0 ? _layout$openAs : 'dropdown'
     };
   } else if (layout?.type === 'card') {
     if (layout.withHeader === false) {
@@ -51816,12 +51817,7 @@ function FormRegularField({
   });
 }
 
-;// ./packages/dataviews/build-module/dataforms-layouts/panel/index.js
-/**
- * External dependencies
- */
-
-
+;// ./packages/dataviews/build-module/dataforms-layouts/panel/dropdown.js
 /**
  * WordPress dependencies
  */
@@ -51833,7 +51829,6 @@ function FormRegularField({
 /**
  * Internal dependencies
  */
-
 
 
 
@@ -51939,6 +51934,150 @@ function PanelDropdown({
     })
   });
 }
+/* harmony default export */ const dropdown = (PanelDropdown);
+
+;// ./packages/dataviews/build-module/dataforms-layouts/panel/modal.js
+/**
+ * WordPress dependencies
+ */
+
+
+
+
+/**
+ * Internal dependencies
+ */
+
+
+
+
+
+function ModalContent({
+  data,
+  form,
+  fieldLabel,
+  onChange,
+  onClose
+}) {
+  const [changes, setChanges] = (0,external_wp_element_namespaceObject.useState)({});
+  const onApply = () => {
+    onChange(changes);
+    onClose();
+  };
+  const handleOnChange = value => {
+    setChanges(prev => ({
+      ...prev,
+      ...value
+    }));
+  };
+
+  // Merge original data with local changes for display
+  const displayData = {
+    ...data,
+    ...changes
+  };
+  return /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsxs)(external_wp_components_namespaceObject.Modal, {
+    className: "dataforms-layouts-panel__modal",
+    onRequestClose: onClose,
+    isFullScreen: false,
+    title: fieldLabel,
+    size: "medium",
+    children: [/*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(DataFormLayout, {
+      data: displayData,
+      form: form,
+      onChange: handleOnChange,
+      children: (FieldLayout, nestedField) => {
+        var _form$fields;
+        return /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(FieldLayout, {
+          data: displayData,
+          field: nestedField,
+          onChange: handleOnChange,
+          hideLabelFromVision: ((_form$fields = form?.fields) !== null && _form$fields !== void 0 ? _form$fields : []).length < 2
+        }, nestedField.id);
+      }
+    }), /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsxs)(external_wp_components_namespaceObject.__experimentalHStack, {
+      className: "dataforms-layouts-panel__modal-footer",
+      spacing: 3,
+      children: [/*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.__experimentalSpacer, {}), /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.Button, {
+        variant: "tertiary",
+        onClick: onClose,
+        __next40pxDefaultSize: true,
+        children: (0,external_wp_i18n_namespaceObject.__)('Cancel')
+      }), /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.Button, {
+        variant: "primary",
+        onClick: onApply,
+        __next40pxDefaultSize: true,
+        children: (0,external_wp_i18n_namespaceObject.__)('Apply')
+      })]
+    })]
+  });
+}
+function PanelModal({
+  fieldDefinition,
+  labelPosition,
+  data,
+  onChange,
+  field
+}) {
+  const [isOpen, setIsOpen] = (0,external_wp_element_namespaceObject.useState)(false);
+  const fieldLabel = isCombinedField(field) ? field.label : fieldDefinition?.label;
+  const form = (0,external_wp_element_namespaceObject.useMemo)(() => ({
+    layout: DEFAULT_LAYOUT,
+    fields: isCombinedField(field) ? field.children :
+    // If not explicit children return the field id itself.
+    [{
+      id: field.id
+    }]
+  }), [field]);
+  return /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsxs)(external_ReactJSXRuntime_namespaceObject.Fragment, {
+    children: [/*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(external_wp_components_namespaceObject.Button, {
+      className: "dataforms-layouts-modal__field-control",
+      size: "compact",
+      variant: ['none', 'top'].includes(labelPosition) ? 'link' : 'tertiary',
+      "aria-expanded": isOpen,
+      "aria-label": (0,external_wp_i18n_namespaceObject.sprintf)(
+      // translators: %s: Field name.
+      (0,external_wp_i18n_namespaceObject._x)('Edit %s', 'field'), fieldLabel || ''),
+      onClick: () => setIsOpen(true),
+      disabled: fieldDefinition.readOnly === true,
+      accessibleWhenDisabled: true,
+      children: /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(fieldDefinition.render, {
+        item: data,
+        field: fieldDefinition
+      })
+    }), isOpen && /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(ModalContent, {
+      data: data,
+      form: form,
+      fieldLabel: fieldLabel !== null && fieldLabel !== void 0 ? fieldLabel : '',
+      onChange: onChange,
+      onClose: () => setIsOpen(false)
+    })]
+  });
+}
+/* harmony default export */ const modal = (PanelModal);
+
+;// ./packages/dataviews/build-module/dataforms-layouts/panel/index.js
+/**
+ * External dependencies
+ */
+
+
+/**
+ * WordPress dependencies
+ */
+
+
+
+/**
+ * Internal dependencies
+ */
+
+
+
+
+
+
+
 function FormPanelField({
   data,
   field,
@@ -51973,6 +52112,20 @@ function FormPanelField({
   const labelPosition = layout.labelPosition;
   const labelClassName = dist_clsx('dataforms-layouts-panel__field-label', `dataforms-layouts-panel__field-label--label-position-${labelPosition}`);
   const fieldLabel = isCombinedField(field) ? field.label : fieldDefinition?.label;
+  const renderedControl = layout.openAs === 'modal' ? /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(modal, {
+    field: field,
+    fieldDefinition: fieldDefinition,
+    data: data,
+    onChange: onChange,
+    labelPosition: labelPosition
+  }) : /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(dropdown, {
+    field: field,
+    popoverAnchor: popoverAnchor,
+    fieldDefinition: fieldDefinition,
+    data: data,
+    onChange: onChange,
+    labelPosition: labelPosition
+  });
   if (labelPosition === 'top') {
     return /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsxs)(external_wp_components_namespaceObject.__experimentalVStack, {
       className: "dataforms-layouts-panel__field",
@@ -51985,28 +52138,14 @@ function FormPanelField({
         children: fieldLabel
       }), /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)("div", {
         className: "dataforms-layouts-panel__field-control",
-        children: /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(PanelDropdown, {
-          field: field,
-          popoverAnchor: popoverAnchor,
-          fieldDefinition: fieldDefinition,
-          data: data,
-          onChange: onChange,
-          labelPosition: labelPosition
-        })
+        children: renderedControl
       })]
     });
   }
   if (labelPosition === 'none') {
     return /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)("div", {
       className: "dataforms-layouts-panel__field",
-      children: /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(PanelDropdown, {
-        field: field,
-        popoverAnchor: popoverAnchor,
-        fieldDefinition: fieldDefinition,
-        data: data,
-        onChange: onChange,
-        labelPosition: labelPosition
-      })
+      children: renderedControl
     });
   }
 
@@ -52019,14 +52158,7 @@ function FormPanelField({
       children: fieldLabel
     }), /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)("div", {
       className: "dataforms-layouts-panel__field-control",
-      children: /*#__PURE__*/(0,external_ReactJSXRuntime_namespaceObject.jsx)(PanelDropdown, {
-        field: field,
-        popoverAnchor: popoverAnchor,
-        fieldDefinition: fieldDefinition,
-        data: data,
-        onChange: onChange,
-        labelPosition: labelPosition
-      })
+      children: renderedControl
     })]
   });
 }
