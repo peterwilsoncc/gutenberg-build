b25029c2d89b578a42ef37554270b0eebe4e4f05
Sarah Norris <1645628+mikachan@users.noreply.github.com>
2025-12-17 14:21:22 +1100

    Template Activation: Make template edit test less flaky again (#74066)
    * Check for publish save button
    
    * Use locator.or helper
    
    Co-authored-by: mikachan <mikachan@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/9d1ddf5a09d7e0c3ba2d14394c1c3b81fcb679c3

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`4792d4175c`](https://github.com/WordPress/gutenberg/commit/4792d4175c15d59702158250a8701d993618dffb), [Browse files](https://github.com/WordPress/gutenberg/tree/4792d4175c15d59702158250a8701d993618dffb)
+* Commit: [WordPress/Gutenberg@`9d1ddf5a09`](https://github.com/WordPress/gutenberg/commit/9d1ddf5a09d7e0c3ba2d14394c1c3b81fcb679c3), [Browse files](https://github.com/WordPress/gutenberg/tree/9d1ddf5a09d7e0c3ba2d14394c1c3b81fcb679c3)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/4792d4175c15d59702158250a8701d993618dffb/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/9d1ddf5a09d7e0c3ba2d14394c1c3b81fcb679c3/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -63,14 +63,14 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-customize-widgets',
-		'path' => 'customize-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-block-library',
 		'path' => 'block-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
+	),
+	array(
+		'handle' => 'wp-customize-widgets',
+		'path' => 'customize-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-block-editor',
@@ -83,14 +83,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences'),
 	),
 	array(
-		'handle' => 'wp-edit-widgets',
-		'path' => 'edit-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-edit-post',
 		'path' => 'edit-post/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-edit-widgets',
+		'path' => 'edit-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-edit-site',
