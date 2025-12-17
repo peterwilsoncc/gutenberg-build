cba1c80469be0cbe1b1f8e8a438d1b1329e8048e
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2025-12-17 15:00:35 +1100

    UI: Remove support for numeric multiplier on Box padding (#74008)
    * UI: Remove support for numeric multiplier on Box padding
    
    * Add CHANGELOG note
    
    * Add PR link to CHANGELOG
    
    * Remove lingering references to number support
    
    * Clarify description of getSpacingValue function
    
    Co-authored-by: Lena Morita <lena@jaguchi.com>
    
    * Use type union for available target grouping
    
    ---------
    
    Co-authored-by: Lena Morita <lena@jaguchi.com>
    
    Co-authored-by: aduth <aduth@git.wordpress.org>
    Co-authored-by: jameskoster <jameskoster@git.wordpress.org>
    Co-authored-by: mirka <0mirka00@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/b66e1781686cc7d67275c1727fb9e8e9a4fe0e92

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`229fe111ed`](https://github.com/WordPress/gutenberg/commit/229fe111ede007a511dc1b5a9218762a0dc7e5ff), [Browse files](https://github.com/WordPress/gutenberg/tree/229fe111ede007a511dc1b5a9218762a0dc7e5ff)
+* Commit: [WordPress/Gutenberg@`b66e178168`](https://github.com/WordPress/gutenberg/commit/b66e1781686cc7d67275c1727fb9e8e9a4fe0e92), [Browse files](https://github.com/WordPress/gutenberg/tree/b66e1781686cc7d67275c1727fb9e8e9a4fe0e92)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/229fe111ede007a511dc1b5a9218762a0dc7e5ff/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/b66e1781686cc7d67275c1727fb9e8e9a4fe0e92/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -33,14 +33,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-commands',
-		'path' => 'commands/style',
-		'dependencies' => array('wp-components'),
-	),
-	array(
 		'handle' => 'wp-widgets',
 		'path' => 'widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
+		'handle' => 'wp-commands',
+		'path' => 'commands/style',
+		'dependencies' => array('wp-components'),
 	),
 	array(
 		'handle' => 'wp-components',
@@ -63,11 +63,6 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-customize-widgets',
-		'path' => 'customize-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-block-library',
 		'path' => 'block-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
@@ -76,6 +71,11 @@ return array(
 		'handle' => 'wp-block-editor',
 		'path' => 'block-editor/style',
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences'),
+	),
+	array(
+		'handle' => 'wp-customize-widgets',
+		'path' => 'customize-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-editor',
