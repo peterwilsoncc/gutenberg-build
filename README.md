7976d65cdcbc76841de97f1809c68e009a3b0f6d
Ramon <ramonjd@users.noreply.github.com>
2025-12-23 03:44:19 +1100

    Block visibility based on screen size: add backend block support (#73994)
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: PrathameshBhagat365 <prathameshbhagat1511@git.wordpress.org>
    Co-authored-by: mtias <matveb@git.wordpress.org>
    Co-authored-by: tellthemachines <isabel_brison@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/0c7e20601ad8d17635b9d50c228cf60192822729

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`7e44e3f670c`](https://github.com/WordPress/gutenberg/commit/7e44e3f670c297c75406bf0b82fd2ecad1fa1958), [Browse files](https://github.com/WordPress/gutenberg/tree/7e44e3f670c297c75406bf0b82fd2ecad1fa1958)
+* Commit: [WordPress/Gutenberg@`0c7e20601ad`](https://github.com/WordPress/gutenberg/commit/0c7e20601ad8d17635b9d50c228cf60192822729), [Browse files](https://github.com/WordPress/gutenberg/tree/0c7e20601ad8d17635b9d50c228cf60192822729)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/7e44e3f670c297c75406bf0b82fd2ecad1fa1958/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/0c7e20601ad8d17635b9d50c228cf60192822729/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -38,14 +38,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
-	),
-	array(
 		'handle' => 'wp-commands',
 		'path' => 'commands/style',
 		'dependencies' => array('wp-components'),
+	),
+	array(
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -53,19 +53,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-media-utils',
-		'path' => 'media-utils/style',
-		'dependencies' => array('wp-components'),
-	),
-	array(
 		'handle' => 'wp-block-directory',
 		'path' => 'block-directory/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
-		'handle' => 'wp-block-library',
-		'path' => 'block-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
+		'handle' => 'wp-media-utils',
+		'path' => 'media-utils/style',
+		'dependencies' => array('wp-components'),
 	),
 	array(
 		'handle' => 'wp-customize-widgets',
@@ -73,9 +68,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-editor',
-		'path' => 'editor/style',
-		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences'),
+		'handle' => 'wp-block-library',
+		'path' => 'block-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-block-editor',
@@ -91,6 +86,11 @@ return array(
 		'handle' => 'wp-edit-widgets',
 		'path' => 'edit-widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-editor',
+		'path' => 'editor/style',
+		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences'),
 	),
 	array(
 		'handle' => 'wp-edit-site',
