87ecb0e1380047e469d98b9a4e5eb17e59164c3b
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2026-01-05 14:27:15 +1100

    Block Support: Fix horizontal overflow in Manage allowed blocks modal (#74337)
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/8fb1eef2067e2c7f5a6d51a4c66f7d23c57c195b

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`066d60c9222`](https://github.com/WordPress/gutenberg/commit/066d60c92223e85b65e3e4e7900bf98ed5ae096a), [Browse files](https://github.com/WordPress/gutenberg/tree/066d60c92223e85b65e3e4e7900bf98ed5ae096a)
+* Commit: [WordPress/Gutenberg@`8fb1eef2067`](https://github.com/WordPress/gutenberg/commit/8fb1eef2067e2c7f5a6d51a4c66f7d23c57c195b), [Browse files](https://github.com/WordPress/gutenberg/tree/8fb1eef2067e2c7f5a6d51a4c66f7d23c57c195b)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/066d60c92223e85b65e3e4e7900bf98ed5ae096a/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/8fb1eef2067e2c7f5a6d51a4c66f7d23c57c195b/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -8,13 +8,13 @@
 
 return array(
 	array(
-		'handle' => 'wp-nux',
-		'path' => 'nux/style',
+		'handle' => 'wp-preferences',
+		'path' => 'preferences/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-preferences',
-		'path' => 'preferences/style',
+		'handle' => 'wp-nux',
+		'path' => 'nux/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
@@ -33,9 +33,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-commands',
@@ -43,9 +43,9 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -63,11 +63,6 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-block-editor',
-		'path' => 'block-editor/style',
-		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences'),
-	),
-	array(
 		'handle' => 'wp-block-library',
 		'path' => 'block-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
@@ -83,14 +78,19 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences'),
 	),
 	array(
+		'handle' => 'wp-edit-widgets',
+		'path' => 'edit-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
 		'handle' => 'wp-edit-post',
 		'path' => 'edit-post/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-edit-widgets',
-		'path' => 'edit-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
+		'handle' => 'wp-block-editor',
+		'path' => 'block-editor/style',
+		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences'),
 	),
 	array(
 		'handle' => 'wp-edit-site',
