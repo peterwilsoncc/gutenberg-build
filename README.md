16b186c72a7f8a2df1ab2b31db6b5cc6d5d7187b
Ryan Welcher <me@ryanwelcher.com>
2026-01-05 13:25:41 +1100

    Update the useCommandLoader example to fix the syntax error and add missing imports. (#73660)
    * Update the example to fix the syntax error and add missing imports.
    
    * Updates per code review.
    
    Co-authored-by: ryanwelcher <welcher@git.wordpress.org>
    Co-authored-by: noruzzamans <noruzzaman@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/869c7ebf7100d12a445a6483796c1d65398733e1

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`ce38bbbe0fa`](https://github.com/WordPress/gutenberg/commit/ce38bbbe0fa06c3bfc3f22e9c2381f6ac6b43db8), [Browse files](https://github.com/WordPress/gutenberg/tree/ce38bbbe0fa06c3bfc3f22e9c2381f6ac6b43db8)
+* Commit: [WordPress/Gutenberg@`869c7ebf710`](https://github.com/WordPress/gutenberg/commit/869c7ebf7100d12a445a6483796c1d65398733e1), [Browse files](https://github.com/WordPress/gutenberg/tree/869c7ebf7100d12a445a6483796c1d65398733e1)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/ce38bbbe0fa06c3bfc3f22e9c2381f6ac6b43db8/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/869c7ebf7100d12a445a6483796c1d65398733e1/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -13,13 +13,13 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-nux',
-		'path' => 'nux/style',
+		'handle' => 'wp-list-reusable-blocks',
+		'path' => 'list-reusable-blocks/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-list-reusable-blocks',
-		'path' => 'list-reusable-blocks/style',
+		'handle' => 'wp-nux',
+		'path' => 'nux/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
@@ -33,14 +33,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
 		'handle' => 'wp-commands',
 		'path' => 'commands/style',
 		'dependencies' => array('wp-components'),
+	),
+	array(
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-components',
@@ -53,14 +53,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-block-directory',
-		'path' => 'block-directory/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
-	),
-	array(
 		'handle' => 'wp-media-utils',
 		'path' => 'media-utils/style',
 		'dependencies' => array('wp-components'),
+	),
+	array(
+		'handle' => 'wp-block-directory',
+		'path' => 'block-directory/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
 		'handle' => 'wp-block-editor',
@@ -68,14 +68,14 @@ return array(
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences'),
 	),
 	array(
-		'handle' => 'wp-customize-widgets',
-		'path' => 'customize-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-block-library',
 		'path' => 'block-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
+	),
+	array(
+		'handle' => 'wp-customize-widgets',
+		'path' => 'customize-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-editor',
