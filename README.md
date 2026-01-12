c896be32dc63a5b5844ba85b8d8a80bc1d5e1e72
Lena Morita <lena@jaguchi.com>
2026-01-12 18:33:01 +1100

    Storybook: Retire old theme switcher (#74499)
    Co-authored-by: mirka <0mirka00@git.wordpress.org>
    Co-authored-by: aduth <aduth@git.wordpress.org>
    Co-authored-by: tyxla <tyxla@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/a3c505629c4bfc708efcdb89ea04bca48fa51dd2

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`943acc08ff`](https://github.com/WordPress/gutenberg/commit/943acc08fffe825f17efd62f8164933ae3cf2518), [Browse files](https://github.com/WordPress/gutenberg/tree/943acc08fffe825f17efd62f8164933ae3cf2518)
+* Commit: [WordPress/Gutenberg@`a3c505629c`](https://github.com/WordPress/gutenberg/commit/a3c505629c4bfc708efcdb89ea04bca48fa51dd2), [Browse files](https://github.com/WordPress/gutenberg/tree/a3c505629c4bfc708efcdb89ea04bca48fa51dd2)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/943acc08fffe825f17efd62f8164933ae3cf2518/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/a3c505629c4bfc708efcdb89ea04bca48fa51dd2/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/modules/index.php
+++ build/modules/index.php
@@ -13,11 +13,6 @@ return array(
 		'asset' => 'interactivity/index.min.asset.php',
 	),
 	array(
-		'id' => '@wordpress/abilities',
-		'path' => 'abilities/index',
-		'asset' => 'abilities/index.min.asset.php',
-	),
-	array(
 		'id' => '@wordpress/latex-to-mathml',
 		'path' => 'latex-to-mathml/index',
 		'asset' => 'latex-to-mathml/index.min.asset.php',
@@ -41,6 +36,11 @@ return array(
 		'id' => '@wordpress/a11y',
 		'path' => 'a11y/index',
 		'asset' => 'a11y/index.min.asset.php',
+	),
+	array(
+		'id' => '@wordpress/abilities',
+		'path' => 'abilities/index',
+		'asset' => 'abilities/index.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/core-abilities',
@@ -71,6 +71,11 @@ return array(
 		'id' => '@wordpress/boot',
 		'path' => 'boot/index',
 		'asset' => 'boot/index.min.asset.php',
+	),
+	array(
+		'id' => '@wordpress/block-editor/utils/fit-text-frontend',
+		'path' => 'block-editor/utils/fit-text-frontend',
+		'asset' => 'block-editor/utils/fit-text-frontend.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/block-library/accordion/view',
@@ -111,11 +116,6 @@ return array(
 		'id' => '@wordpress/block-library/tabs/view',
 		'path' => 'block-library/tabs/view',
 		'asset' => 'block-library/tabs/view.min.asset.php',
-	),
-	array(
-		'id' => '@wordpress/block-editor/utils/fit-text-frontend',
-		'path' => 'block-editor/utils/fit-text-frontend',
-		'asset' => 'block-editor/utils/fit-text-frontend.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/boot',
