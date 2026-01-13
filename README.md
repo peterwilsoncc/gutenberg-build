4baa98dbfef77c1a79c69f2c19d94cd6fc5ad63b
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2026-01-14 08:42:47 +1100

    Update Storybook to v10 with Vite builder (#74396)
    * Update Storybook to v10 with Vite builder
    
    * Update TypeScript base config to use bundler module resolution
    
    * Update storybook to v10: Misc updates (#74491)
    
    * Use preset for addon
    
    * Convert main.mjs to main.ts
    
    * Disable default links for v10
    
    * Update Storybook test-runner to v0.24.2
    
    * Remove webpack from ignored licenses list
    
    Co-authored-by: manzoorwanijk <manzoorwanijk@git.wordpress.org>
    Co-authored-by: aduth <aduth@git.wordpress.org>
    
    * Avoid variable shadowing by type-checking as satisfies
    
    * Align types export on jest-console package
    
    * Update E2E Storybook configuration for v10
    
    * Fix file extension on README Storybook configuration file reference
    
    Co-Authored-By: Manzoor Wani <18226415+manzoorwanijk@users.noreply.github.com>
    
    * Use `?inline` instead of `?raw` for importing compiled Sass
    
    `?raw` imports raw content of the referenced file. `?inline` assigns "the processed CSS string"
    
    See: https://vite.dev/guide/features#disabling-css-injection-into-the-page
    
    * Try renaming JavaScript Story to TypeScript
    
    * Pin user-event to 14.4.3
    
    Newer versions have an issue preventing reassignment of `HTMLElement.prototype.focus`, which is fixed in source, but not published in an available release. The reassignment is part of Ariakit's testing polyfills.
    
    See:
    
    - https://github.com/testing-library/user-event/pull/1265
    - https://github.com/ariakit/ariakit/blob/016f8a3ee9d3e59c593e2971221111a7fa137727/packages/ariakit-test/src/__utils.ts#L34
    
    * Sync Storybook dependencies at common version
    
    Also removes stale and conflicting entries in package-lock.json
    
    * Update reference to storybook/react-webpack5 dependency
    
    * Fix Storybook build by updating README import in introduction files
    
    Update the import statements in introduction.mdx files to use '?raw' when importing README.md.
    
    * Remove moduleResolution from sync tsconfig
    
    * Add missing dependency array to useEffect
    
    * Remove unnecessary .lazy suffix from scss files
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    Co-authored-by: manzoorwanijk <manzoorwanijk@git.wordpress.org>
    Co-authored-by: Manzoor Wani <18226415+manzoorwanijk@users.noreply.github.com>
    
    Co-authored-by: aduth <aduth@git.wordpress.org>
    Co-authored-by: manzoorwanijk <manzoorwanijk@git.wordpress.org>
    Co-authored-by: jsnajdr <jsnajdr@git.wordpress.org>
    Co-authored-by: mirka <0mirka00@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/20d91613a29204003858ba50a4f1534ea28790e3

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`ac8c90f985`](https://github.com/WordPress/gutenberg/commit/ac8c90f985bea5505e3d0e034f7e383f442f4ae8), [Browse files](https://github.com/WordPress/gutenberg/tree/ac8c90f985bea5505e3d0e034f7e383f442f4ae8)
+* Commit: [WordPress/Gutenberg@`20d91613a2`](https://github.com/WordPress/gutenberg/commit/20d91613a29204003858ba50a4f1534ea28790e3), [Browse files](https://github.com/WordPress/gutenberg/tree/20d91613a29204003858ba50a4f1534ea28790e3)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/ac8c90f985bea5505e3d0e034f7e383f442f4ae8/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/20d91613a29204003858ba50a4f1534ea28790e3/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -28,11 +28,6 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
 		'handle' => 'wp-patterns',
 		'path' => 'patterns/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
@@ -43,14 +38,19 @@ return array(
 		'dependencies' => array(),
 	),
 	array(
-		'handle' => 'wp-format-library',
-		'path' => 'format-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
 		'handle' => 'wp-commands',
 		'path' => 'commands/style',
 		'dependencies' => array('wp-components'),
+	),
+	array(
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
+		'handle' => 'wp-format-library',
+		'path' => 'format-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-block-directory',
@@ -58,9 +58,19 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
+		'handle' => 'wp-media-utils',
+		'path' => 'media-utils/style',
+		'dependencies' => array('wp-components'),
+	),
+	array(
 		'handle' => 'wp-block-editor',
 		'path' => 'block-editor/style',
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences'),
+	),
+	array(
+		'handle' => 'wp-block-library',
+		'path' => 'block-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-customize-widgets',
@@ -68,19 +78,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-media-utils',
-		'path' => 'media-utils/style',
-		'dependencies' => array('wp-components'),
-	),
-	array(
 		'handle' => 'wp-editor',
 		'path' => 'editor/style',
 		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences'),
-	),
-	array(
-		'handle' => 'wp-block-library',
-		'path' => 'block-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-edit-post',
