6ca0fbdc12b58c6fdaab5c5c0de7183e9a969006
André <583546+oandregal@users.noreply.github.com>
2026-01-14 11:07:56 +1100

    DataViews stories: add custom layout (#74605)
    Source: https://github.com/WordPress/gutenberg/commit/51ecdd715ce3f169354a29242abb0b4f974b37fb

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`dba50685f4`](https://github.com/WordPress/gutenberg/commit/dba50685f469aecd99777a7d447a03175f5bdf72), [Browse files](https://github.com/WordPress/gutenberg/tree/dba50685f469aecd99777a7d447a03175f5bdf72)
+* Commit: [WordPress/Gutenberg@`51ecdd715c`](https://github.com/WordPress/gutenberg/commit/51ecdd715ce3f169354a29242abb0b4f974b37fb), [Browse files](https://github.com/WordPress/gutenberg/tree/51ecdd715ce3f169354a29242abb0b4f974b37fb)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/dba50685f469aecd99777a7d447a03175f5bdf72/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/51ecdd715ce3f169354a29242abb0b4f974b37fb/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -23,13 +23,18 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
+		'handle' => 'wp-reusable-blocks',
+		'path' => 'reusable-blocks/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
 		'handle' => 'wp-patterns',
 		'path' => 'patterns/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-reusable-blocks',
-		'path' => 'reusable-blocks/style',
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
@@ -38,19 +43,14 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-format-library',
 		'path' => 'format-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-block-directory',
@@ -68,14 +68,14 @@ return array(
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences'),
 	),
 	array(
-		'handle' => 'wp-customize-widgets',
-		'path' => 'customize-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-block-library',
 		'path' => 'block-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
+	),
+	array(
+		'handle' => 'wp-customize-widgets',
+		'path' => 'customize-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-editor',
