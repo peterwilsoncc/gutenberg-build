4f8e3d79977363c802473f2a04d2a20193693e5b
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2026-01-14 13:43:13 +1100

    Fix deprecations for Storybook component usage (#74619)
    * Fix deprecations for Storybook component usage
    
    * Add custom Storybook addon for source link
    
    * Remove a return path in favor of optional path access
    
    * Remove redundant aria-hidden
    
    Parent button already assigns aria-label
    
    Co-authored-by: aduth <aduth@git.wordpress.org>
    Co-authored-by: manzoorwanijk <manzoorwanijk@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/a813786e8e43271eff3cb9c18637567de7f5278a

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`85417abfd8`](https://github.com/WordPress/gutenberg/commit/85417abfd84d2fa6f0562442b285a06b748dbf5e), [Browse files](https://github.com/WordPress/gutenberg/tree/85417abfd84d2fa6f0562442b285a06b748dbf5e)
+* Commit: [WordPress/Gutenberg@`a813786e8e`](https://github.com/WordPress/gutenberg/commit/a813786e8e43271eff3cb9c18637567de7f5278a), [Browse files](https://github.com/WordPress/gutenberg/tree/a813786e8e43271eff3cb9c18637567de7f5278a)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/85417abfd84d2fa6f0562442b285a06b748dbf5e/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/a813786e8e43271eff3cb9c18637567de7f5278a/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/index.php
+++ build/styles/index.php
@@ -13,13 +13,13 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-list-reusable-blocks',
-		'path' => 'list-reusable-blocks/style',
+		'handle' => 'wp-nux',
+		'path' => 'nux/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-nux',
-		'path' => 'nux/style',
+		'handle' => 'wp-list-reusable-blocks',
+		'path' => 'list-reusable-blocks/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
@@ -43,14 +43,14 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-format-library',
-		'path' => 'format-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
 		'handle' => 'wp-components',
 		'path' => 'components/style',
 		'dependencies' => array(),
+	),
+	array(
+		'handle' => 'wp-format-library',
+		'path' => 'format-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-block-directory',
@@ -78,6 +78,11 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
+		'handle' => 'wp-edit-post',
+		'path' => 'edit-post/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
 		'handle' => 'wp-editor',
 		'path' => 'editor/style',
 		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences'),
@@ -86,11 +91,6 @@ return array(
 		'handle' => 'wp-edit-widgets',
 		'path' => 'edit-widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
-		'handle' => 'wp-edit-post',
-		'path' => 'edit-post/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-edit-site',
