3935901a4c5f0de2593ca960eb481f56b36a3c32
Mustafa Bharmal <92981596+Mustafabharmal@users.noreply.github.com>
2026-01-22 15:31:02 +1100

    Fix: Escape less-than character in HTML attributes to prevent block recovery errors (#74732)
    * Fix escaping of less-than symbol in attribute values to prevent HTML injection vulnerabilities
    
    * Fix escaping of less-than symbol in INVALID_HTML_ATTRIBUTE
    
    Source: https://github.com/WordPress/gutenberg/commit/8c7f99a773ed4d9b9b9af07cce6f10af3e17b70e

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`ad4ec34bac4`](https://github.com/WordPress/gutenberg/commit/ad4ec34bac4106262ed604e0053069f8d3e7c3c7), [Browse files](https://github.com/WordPress/gutenberg/tree/ad4ec34bac4106262ed604e0053069f8d3e7c3c7)
+* Commit: [WordPress/Gutenberg@`8c7f99a773e`](https://github.com/WordPress/gutenberg/commit/8c7f99a773ed4d9b9b9af07cce6f10af3e17b70e), [Browse files](https://github.com/WordPress/gutenberg/tree/8c7f99a773ed4d9b9b9af07cce6f10af3e17b70e)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/ad4ec34bac4106262ed604e0053069f8d3e7c3c7/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/8c7f99a773ed4d9b9b9af07cce6f10af3e17b70e/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/registry.php
+++ build/styles/registry.php
@@ -38,9 +38,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-commands',
@@ -48,9 +48,9 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -63,19 +63,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
-		'handle' => 'wp-media-utils',
-		'path' => 'media-utils/style',
-		'dependencies' => array('wp-components', 'wp-theme'),
-	),
-	array(
 		'handle' => 'wp-block-editor',
 		'path' => 'block-editor/style',
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences', 'wp-theme'),
 	),
 	array(
-		'handle' => 'wp-block-library',
-		'path' => 'block-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
+		'handle' => 'wp-media-utils',
+		'path' => 'media-utils/style',
+		'dependencies' => array('wp-components', 'wp-theme'),
 	),
 	array(
 		'handle' => 'wp-customize-widgets',
@@ -83,9 +78,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-edit-post',
-		'path' => 'edit-post/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
+		'handle' => 'wp-block-library',
+		'path' => 'block-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-edit-widgets',
@@ -93,13 +88,18 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-editor',
-		'path' => 'editor/style',
-		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-theme'),
+		'handle' => 'wp-edit-post',
+		'path' => 'edit-post/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-edit-site',
 		'path' => 'edit-site/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-patterns', 'wp-preferences', 'wp-theme', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-editor',
+		'path' => 'editor/style',
+		'dependencies' => array('wp-block-editor', 'wp-commands', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-theme'),
 	),
 );
