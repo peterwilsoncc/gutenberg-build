1bca5644c82e08aa8a17a2a8db0b4655bfb3bc5b
Luis Herranz <luisherranz@gmail.com>
2026-01-27 10:39:38 +1100

    iAPI Docs: add config to state/context guide (#71355)
    * iAPI Docs: add config to state/context guide
    
    * Update docs/reference-guides/interactivity-api/core-concepts/undestanding-global-state-local-context-and-derived-state.md
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    
    ---------
    Co-authored-by: luisherranz <luisherranz@git.wordpress.org>
    Co-authored-by: obenland <obenland@git.wordpress.org>
    Co-authored-by: DAreRodz <darerodz@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/00a60dc2de5fe81554cd0668e4886135e0c9e867

--- build/modules/registry.php
+++ build/modules/registry.php
@@ -23,6 +23,11 @@ return array(
 		'asset' => 'latex-to-mathml/loader.min.asset.php',
 	),
 	array(
+		'id' => '@wordpress/a11y',
+		'path' => 'a11y/index',
+		'asset' => 'a11y/index.min.asset.php',
+	),
+	array(
 		'id' => '@wordpress/interactivity-router',
 		'path' => 'interactivity-router/index',
 		'asset' => 'interactivity-router/index.min.asset.php',
@@ -38,19 +43,14 @@ return array(
 		'asset' => 'abilities/index.min.asset.php',
 	),
 	array(
-		'id' => '@wordpress/a11y',
-		'path' => 'a11y/index',
-		'asset' => 'a11y/index.min.asset.php',
+		'id' => '@wordpress/route',
+		'path' => 'route/index',
+		'asset' => 'route/index.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/core-abilities',
 		'path' => 'core-abilities/index',
 		'asset' => 'core-abilities/index.min.asset.php',
-	),
-	array(
-		'id' => '@wordpress/route',
-		'path' => 'route/index',
-		'asset' => 'route/index.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/edit-site-init',
@@ -71,6 +71,11 @@ return array(
 		'id' => '@wordpress/boot',
 		'path' => 'boot/index',
 		'asset' => 'boot/index.min.asset.php',
+	),
+	array(
+		'id' => '@wordpress/block-editor/utils/fit-text-frontend',
+		'path' => 'block-editor/utils/fit-text-frontend',
+		'asset' => 'block-editor/utils/fit-text-frontend.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/block-library/accordion/view',
@@ -111,11 +116,6 @@ return array(
 		'id' => '@wordpress/block-library/tabs/view',
 		'path' => 'block-library/tabs/view',
 		'asset' => 'block-library/tabs/view.min.asset.php',
-	),
-	array(
-		'id' => '@wordpress/block-editor/utils/fit-text-frontend',
-		'path' => 'block-editor/utils/fit-text-frontend',
-		'asset' => 'block-editor/utils/fit-text-frontend.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/boot',
