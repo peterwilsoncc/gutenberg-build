65aa6f817bcda8a2458512edfd96bb3116b2b4c7
Noruzzaman <noruzzamanrubel@gmail.com>
2026-01-30 16:16:13 +1100

    Docs: Add missing @global documentation in rtl.php and meta-box.php (#75082)
    Co-authored-by: noruzzamans <noruzzaman@git.wordpress.org>
    Co-authored-by: jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/ae58ba323b7dc4b431fdf6b95fa50bf5c8cdff78

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`8bbda478a28`](https://github.com/WordPress/gutenberg/commit/8bbda478a281c8b8b1787bed81443d6f5ca5d922), [Browse files](https://github.com/WordPress/gutenberg/tree/8bbda478a281c8b8b1787bed81443d6f5ca5d922)
+* Commit: [WordPress/Gutenberg@`ae58ba323b7`](https://github.com/WordPress/gutenberg/commit/ae58ba323b7dc4b431fdf6b95fa50bf5c8cdff78), [Browse files](https://github.com/WordPress/gutenberg/tree/ae58ba323b7dc4b431fdf6b95fa50bf5c8cdff78)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/8bbda478a281c8b8b1787bed81443d6f5ca5d922/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/ae58ba323b7dc4b431fdf6b95fa50bf5c8cdff78/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/registry.php
+++ build/styles/registry.php
@@ -38,9 +38,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
+		'handle' => 'wp-commands',
+		'path' => 'commands/style',
+		'dependencies' => array('wp-components'),
 	),
 	array(
 		'handle' => 'wp-widgets',
@@ -48,9 +48,9 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-commands',
-		'path' => 'commands/style',
-		'dependencies' => array('wp-components'),
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -78,14 +78,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-block-library',
-		'path' => 'block-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
-	),
-	array(
 		'handle' => 'wp-edit-widgets',
 		'path' => 'edit-widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-block-library',
+		'path' => 'block-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-edit-post',
