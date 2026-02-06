04eb428327a5f7396fb902086d23bec9d0e62a87
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2026-02-06 20:24:04 +1100

    Fix awareness timeout documentation unit (#75284)
    Updated awareness timeout constant documentation to reflect seconds instead of milliseconds.
    
    Co-authored-by: pkevan <paulkevan@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: chriszarate <czarate@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/24f6331d07fd01ccf8cd27eca5f371d343908d3c

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`f0290c63852`](https://github.com/WordPress/gutenberg/commit/f0290c638523474238c70632b7a741ab744db044), [Browse files](https://github.com/WordPress/gutenberg/tree/f0290c638523474238c70632b7a741ab744db044)
+* Commit: [WordPress/Gutenberg@`24f6331d07f`](https://github.com/WordPress/gutenberg/commit/24f6331d07fd01ccf8cd27eca5f371d343908d3c), [Browse files](https://github.com/WordPress/gutenberg/tree/24f6331d07fd01ccf8cd27eca5f371d343908d3c)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/f0290c638523474238c70632b7a741ab744db044/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/24f6331d07fd01ccf8cd27eca5f371d343908d3c/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/registry.php
+++ build/styles/registry.php
@@ -38,11 +38,6 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
-	),
-	array(
 		'handle' => 'wp-commands',
 		'path' => 'commands/style',
 		'dependencies' => array('wp-components'),
@@ -53,9 +48,19 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
+	),
+	array(
 		'handle' => 'wp-format-library',
 		'path' => 'format-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
+		'handle' => 'wp-block-editor',
+		'path' => 'block-editor/style',
+		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences', 'wp-theme'),
 	),
 	array(
 		'handle' => 'wp-block-directory',
@@ -63,19 +68,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
-		'handle' => 'wp-media-utils',
-		'path' => 'media-utils/style',
-		'dependencies' => array('wp-components', 'wp-theme'),
-	),
-	array(
 		'handle' => 'wp-customize-widgets',
 		'path' => 'customize-widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
-		'handle' => 'wp-block-editor',
-		'path' => 'block-editor/style',
-		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences', 'wp-theme'),
+		'handle' => 'wp-media-utils',
+		'path' => 'media-utils/style',
+		'dependencies' => array('wp-components', 'wp-theme'),
 	),
 	array(
 		'handle' => 'wp-block-library',
