b9f1c4be6adf68a8a3e14fbd5e21341ed12314b9
David Arenas <david.arenas@automattic.com>
2026-02-09 23:08:38 +1100

    iAPI Router: Update cached styles for re-fetched pages (#75097)
    * Add failing e2e test
    
    * Remove redundant style cache
    
    * Update e2e test comments.
    
    * Update unit tests
    
    * Add note to `preloadStyles`
    
    * Update changelog
    
    Co-authored-by: DAreRodz <darerodz@git.wordpress.org>
    Co-authored-by: luisherranz <luisherranz@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/037f31fa5d6902fbb3ba7184b452963f2863bfad

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`2472df3b947`](https://github.com/WordPress/gutenberg/commit/2472df3b94769892778f9f396b5d8607caeac554), [Browse files](https://github.com/WordPress/gutenberg/tree/2472df3b94769892778f9f396b5d8607caeac554)
+* Commit: [WordPress/Gutenberg@`037f31fa5d6`](https://github.com/WordPress/gutenberg/commit/037f31fa5d6902fbb3ba7184b452963f2863bfad), [Browse files](https://github.com/WordPress/gutenberg/tree/037f31fa5d6902fbb3ba7184b452963f2863bfad)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/2472df3b94769892778f9f396b5d8607caeac554/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/037f31fa5d6902fbb3ba7184b452963f2863bfad/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/modules/interactivity-router/index.js
+++ build/modules/interactivity-router/index.js
@@ -121,24 +121,16 @@ var prepareStylePromise = (element) => {
   stylePromiseCache.set(element, promise);
   return promise;
 };
-var styleSheetCache = /* @__PURE__ */ new Map();
-var preloadStyles = (doc, url) => {
-  if (!styleSheetCache.has(url)) {
-    const currentStyleElements = Array.from(
-      window.document.querySelectorAll(
-        "style,link[rel=stylesheet]"
-      )
-    );
-    const newStyleElements = Array.from(
-      doc.querySelectorAll("style,link[rel=stylesheet]")
-    );
-    const stylePromises = updateStylesWithSCS(
-      currentStyleElements,
-      newStyleElements
-    );
-    styleSheetCache.set(url, stylePromises);
-  }
-  return styleSheetCache.get(url);
+var preloadStyles = (doc) => {
+  const currentStyleElements = Array.from(
+    window.document.querySelectorAll(
+      "style,link[rel=stylesheet]"
+    )
+  );
+  const newStyleElements = Array.from(
+    doc.querySelectorAll("style,link[rel=stylesheet]")
+  );
+  return updateStylesWithSCS(currentStyleElements, newStyleElements);
 };
 var applyStyles = (styles) => {
   window.document.querySelectorAll("style,link[rel=stylesheet]").forEach((el) => {
@@ -750,7 +742,7 @@ var preparePage = async (url, dom, { vdom } = {}) => {
   const title = dom.querySelector("title")?.innerText;
   const initialData = parseServerData(dom);
   const [styles, scriptModules] = await Promise.all([
-    Promise.all(preloadStyles(dom, url)),
+    Promise.all(preloadStyles(dom)),
     Promise.all(preloadScriptModules(dom))
   ]);
   return {
