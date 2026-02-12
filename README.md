04269295e42e3cba5bb19eb2484542d6c9f295b8
Lena Morita <lena@jaguchi.com>
2026-02-13 05:33:09 +1100

    Fix missed dimension token migration in UI package (#75446)
    * Fix missed dimension token migration in UI package
    
    * Add changelog
    
    Co-authored-by: mirka <0mirka00@git.wordpress.org>
    Co-authored-by: aduth <aduth@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/49cdebfd4a4f1650623c419fc8c1193d76abb0c5

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`83fc881553e`](https://github.com/WordPress/gutenberg/commit/83fc881553e0137aa40305d22abbfe3d2cddc488), [Browse files](https://github.com/WordPress/gutenberg/tree/83fc881553e0137aa40305d22abbfe3d2cddc488)
+* Commit: [WordPress/Gutenberg@`49cdebfd4a4`](https://github.com/WordPress/gutenberg/commit/49cdebfd4a4f1650623c419fc8c1193d76abb0c5), [Browse files](https://github.com/WordPress/gutenberg/tree/49cdebfd4a4f1650623c419fc8c1193d76abb0c5)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/83fc881553e0137aa40305d22abbfe3d2cddc488/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/49cdebfd4a4f1650623c419fc8c1193d76abb0c5/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/registry.php
+++ build/styles/registry.php
@@ -43,14 +43,14 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
 		'handle' => 'wp-components',
 		'path' => 'components/style',
 		'dependencies' => array(),
+	),
+	array(
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -63,24 +63,19 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
-		'handle' => 'wp-media-utils',
-		'path' => 'media-utils/style',
-		'dependencies' => array('wp-components', 'wp-theme'),
-	),
-	array(
 		'handle' => 'wp-block-editor',
 		'path' => 'block-editor/style',
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences', 'wp-theme'),
 	),
 	array(
+		'handle' => 'wp-media-utils',
+		'path' => 'media-utils/style',
+		'dependencies' => array('wp-components', 'wp-theme'),
+	),
+	array(
 		'handle' => 'wp-customize-widgets',
 		'path' => 'customize-widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
-		'handle' => 'wp-block-library',
-		'path' => 'block-library/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-edit-post',
@@ -91,6 +86,11 @@ return array(
 		'handle' => 'wp-edit-widgets',
 		'path' => 'edit-widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-block-library',
+		'path' => 'block-library/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
 		'handle' => 'wp-editor',
