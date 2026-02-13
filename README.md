fac92b0409b9921cfac0867c1d66e71ae57e53e8
Jarda Snajdr <jsnajdr@gmail.com>
2026-02-13 15:57:43 +1100

    Story types: fix StoryFns used as React components (#75472)
    Source: https://github.com/WordPress/gutenberg/commit/40c65afb37ada3b04a9f3f889a0f536636b23371

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`4f2d1bc738f`](https://github.com/WordPress/gutenberg/commit/4f2d1bc738f2f88261ebd2928725ce00ac233185), [Browse files](https://github.com/WordPress/gutenberg/tree/4f2d1bc738f2f88261ebd2928725ce00ac233185)
+* Commit: [WordPress/Gutenberg@`40c65afb37a`](https://github.com/WordPress/gutenberg/commit/40c65afb37ada3b04a9f3f889a0f536636b23371), [Browse files](https://github.com/WordPress/gutenberg/tree/40c65afb37ada3b04a9f3f889a0f536636b23371)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/4f2d1bc738f2f88261ebd2928725ce00ac233185/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/40c65afb37ada3b04a9f3f889a0f536636b23371/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/registry.php
+++ build/styles/registry.php
@@ -38,6 +38,11 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
 		'handle' => 'wp-components',
 		'path' => 'components/style',
 		'dependencies' => array(),
@@ -46,11 +51,6 @@ return array(
 		'handle' => 'wp-commands',
 		'path' => 'commands/style',
 		'dependencies' => array('wp-components'),
-	),
-	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -63,14 +63,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
-		'handle' => 'wp-media-utils',
-		'path' => 'media-utils/style',
-		'dependencies' => array('wp-components', 'wp-theme'),
-	),
-	array(
 		'handle' => 'wp-block-editor',
 		'path' => 'block-editor/style',
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences', 'wp-theme'),
+	),
+	array(
+		'handle' => 'wp-media-utils',
+		'path' => 'media-utils/style',
+		'dependencies' => array('wp-components', 'wp-theme'),
 	),
 	array(
 		'handle' => 'wp-customize-widgets',
@@ -83,14 +83,14 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
 	),
 	array(
-		'handle' => 'wp-edit-widgets',
-		'path' => 'edit-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-edit-post',
 		'path' => 'edit-post/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-edit-widgets',
+		'path' => 'edit-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-editor',
