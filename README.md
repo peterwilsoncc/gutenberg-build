65a3b492f6e57957b03325984ba399f7960efe8b
Ramon <ramonjd@users.noreply.github.com>
2026-02-18 09:41:55 +1100

    Update Navigation block tests to use non-deprecated API (#75660)
    * Refactor navigation block tests to use the updated function for block type verification
    * Update test assertions to use block_core_navigation_block_tree_has_block_type instead of the deprecated block_core_navigation_block_contains_core_navigation.
    * Add coverage annotations for the new function in the test cases.
    * Fix `@covers` annotations
    
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    
    ---------
    
    Co-authored-by: George Mamadashvili <georgemamadashvili@gmail.com>
    
    Source: https://github.com/WordPress/gutenberg/commit/810847b084506daf342b63b6c8f1966a36c02772

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `trunk`
-* Commit: [WordPress/Gutenberg@`dd57bcce40e`](https://github.com/WordPress/gutenberg/commit/dd57bcce40e6045fdfa05fae7d336d65865810cb), [Browse files](https://github.com/WordPress/gutenberg/tree/dd57bcce40e6045fdfa05fae7d336d65865810cb)
+* Commit: [WordPress/Gutenberg@`810847b0845`](https://github.com/WordPress/gutenberg/commit/810847b084506daf342b63b6c8f1966a36c02772), [Browse files](https://github.com/WordPress/gutenberg/tree/810847b084506daf342b63b6c8f1966a36c02772)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/dd57bcce40e6045fdfa05fae7d336d65865810cb/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/810847b084506daf342b63b6c8f1966a36c02772/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/modules/registry.php
+++ build/modules/registry.php
@@ -13,16 +13,6 @@ return array(
 		'asset' => 'interactivity/index.min.asset.php',
 	),
 	array(
-		'id' => '@wordpress/a11y',
-		'path' => 'a11y/index',
-		'asset' => 'a11y/index.min.asset.php',
-	),
-	array(
-		'id' => '@wordpress/abilities',
-		'path' => 'abilities/index',
-		'asset' => 'abilities/index.min.asset.php',
-	),
-	array(
 		'id' => '@wordpress/latex-to-mathml',
 		'path' => 'latex-to-mathml/index',
 		'asset' => 'latex-to-mathml/index.min.asset.php',
@@ -31,6 +21,11 @@ return array(
 		'id' => '@wordpress/latex-to-mathml/loader',
 		'path' => 'latex-to-mathml/loader',
 		'asset' => 'latex-to-mathml/loader.min.asset.php',
+	),
+	array(
+		'id' => '@wordpress/a11y',
+		'path' => 'a11y/index',
+		'asset' => 'a11y/index.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/vips/worker',
@@ -51,6 +46,11 @@ return array(
 		'id' => '@wordpress/interactivity-router/full-page',
 		'path' => 'interactivity-router/full-page',
 		'asset' => 'interactivity-router/full-page.min.asset.php',
+	),
+	array(
+		'id' => '@wordpress/abilities',
+		'path' => 'abilities/index',
+		'asset' => 'abilities/index.min.asset.php',
 	),
 	array(
 		'id' => '@wordpress/core-abilities',
