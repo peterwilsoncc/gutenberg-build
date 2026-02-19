6e07fe46a02792bf3d6bec7ee251eef0af1b9d14
Daniel Richards <daniel.richards@automattic.com>
2026-02-20 07:55:15 +1100

    Query: Remove content role from block (#75760)
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: tellthemachines <isabel_brison@git.wordpress.org>
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    
    Source: https://github.com/WordPress/gutenberg/commit/ed9fbde87fd11b9aac4f3a2442d5932948b52ee4

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ This is a duplicate of the [WordPress/Gutenberg](https://github.com/WordPress/gu
 You are currently viewing the version build from the source:
 
 * Branch: `wp/7.0`
-* Commit: [WordPress/Gutenberg@`87ba15ec1a5`](https://github.com/WordPress/gutenberg/commit/87ba15ec1a5f28648563a7a03fcc6e3b0bb77c96), [Browse files](https://github.com/WordPress/gutenberg/tree/87ba15ec1a5f28648563a7a03fcc6e3b0bb77c96)
+* Commit: [WordPress/Gutenberg@`ed9fbde87fd`](https://github.com/WordPress/gutenberg/commit/ed9fbde87fd11b9aac4f3a2442d5932948b52ee4), [Browse files](https://github.com/WordPress/gutenberg/tree/ed9fbde87fd11b9aac4f3a2442d5932948b52ee4)
 
 > [!NOTE]
 > This readme file has replaced the original version included in the upstream repository.
 >
-> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/87ba15ec1a5f28648563a7a03fcc6e3b0bb77c96/README.md) file for this commit can be found in the upstream repository.
+> The [original `README.md` file](https://github.com/WordPress/gutenberg/blob/ed9fbde87fd11b9aac4f3a2442d5932948b52ee4/README.md) file for this commit can be found in the upstream repository.
 
 ## Pupose
 
--- build/styles/registry.php
+++ build/styles/registry.php
@@ -28,13 +28,18 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
+		'handle' => 'wp-reusable-blocks',
+		'path' => 'reusable-blocks/style',
+		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
 		'handle' => 'wp-patterns',
 		'path' => 'patterns/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
-		'handle' => 'wp-reusable-blocks',
-		'path' => 'reusable-blocks/style',
+		'handle' => 'wp-widgets',
+		'path' => 'widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
 	),
 	array(
@@ -48,19 +53,9 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-widgets',
-		'path' => 'widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
 		'handle' => 'wp-format-library',
 		'path' => 'format-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
-	),
-	array(
-		'handle' => 'wp-block-directory',
-		'path' => 'block-directory/style',
-		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
 		'handle' => 'wp-block-editor',
@@ -68,19 +63,24 @@ return array(
 		'dependencies' => array('wp-commands', 'wp-components', 'wp-preferences', 'wp-theme'),
 	),
 	array(
+		'handle' => 'wp-block-directory',
+		'path' => 'block-directory/style',
+		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
+	),
+	array(
 		'handle' => 'wp-media-utils',
 		'path' => 'media-utils/style',
 		'dependencies' => array('wp-components', 'wp-theme'),
 	),
 	array(
-		'handle' => 'wp-customize-widgets',
-		'path' => 'customize-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-block-library',
 		'path' => 'block-library/style',
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-patterns'),
+	),
+	array(
+		'handle' => 'wp-customize-widgets',
+		'path' => 'customize-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-edit-widgets',
